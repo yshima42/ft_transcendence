96232fc207bffabf9ce67b388237445b3541b037
yuki <shimazuyuki7@gmail.com>
2022-10-31 18:11:45 +0900

    chore: eslint

--- README.md
+++ README.md
@@ -3,11 +3,5 @@
 yarn dev
 ```
 
-### 環境構築で行ったこと
-```bash
-# baseUrlのオプションを追加するため
-yarn add -D vite-tsconfig-paths
-```
-
 ### 参考
 - りあクト！ 1章、4章、6章
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_TITLE: string
   // ここに定義した変数を追加していく
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
 }
