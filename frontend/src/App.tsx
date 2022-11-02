81538cca43c3421bb8c3e4fe930ebd631606d6d5
yuki <shimazuyuki7@gmail.com>
2022-11-02 17:39:48 +0900

    chore: 全体themeの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from 'router/Router';
+import theme from './theme/theme';
 
 const App: FC = () => {
   return (
     <div className="App">
       <h1>ft_transcendence</h1>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 };
