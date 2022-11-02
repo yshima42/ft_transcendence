c9047b8591a5ef41e40d8565edc906a7b25bc09f
yuki <shimazuyuki7@gmail.com>
2022-11-02 23:27:30 +0900

    chore: コンソールにてログインデータ取得(アクセストークは自前で取ってきて試せる)

--- frontend/api/access_token_request.sh
+++ frontend/api/access_token_request.sh
@@ -1,6 +1,6 @@
 curl -F grant_type=authorization_code \
 -F client_id=13048d1985df54479741344156321e0b0d101970ad8c152c72f3de8c508c00a2 \
 -F client_secret=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 \
--F code=51b3ffd748d3b3a274247ea8a2e8ed2a387c91120208701c7d0eed4f4cb92854 \
+-F code=1ec9adee482186608f021b228241cc847b86a3fcd175075ebc3752c27a9559fd \
 -F redirect_uri=https://42tokyo.jp \
 -X POST https://api.intra.42.fr/oauth/token | jq
--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,8 +1,24 @@
 import { memo, FC } from 'react';
 import { Box, Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
-import { PrimaryButton } from 'components/atoms/button/PrimaryButton';
+import axios from 'axios';
+// import { PrimaryButton } from 'components/atoms/button/PrimaryButton';
 
 export const Login: FC = memo(() => {
+  const API_URL = 'https://api.intra.42.fr/v2/me';
+  const ACCESS_TOKEN =
+    '012bf266174087b6318c2cf1526c914cf5d033b58183df72bb724ffdf5badfb7';
+
+  const onClickAuth = () => {
+    axios
+      .get(API_URL, { headers: { Authorization: 'Bearer ' + ACCESS_TOKEN } })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log('err: ', err);
+      });
+  };
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -11,7 +27,7 @@ export const Login: FC = memo(() => {
         </Heading>
         <Divider />
         <Stack spacing={4} py={4} px={10}>
-          <PrimaryButton>42ユーザー認証</PrimaryButton>
+          <Button onClick={onClickAuth}>42ユーザー認証</Button>
           <Button>アドミンテスト1</Button>
           <Button>アドミンテスト2</Button>
           <Button>アドミンテスト3</Button>
