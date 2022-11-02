509d7384fb271e71749d424c28c210d85c20899f
yuki <shimazuyuki7@gmail.com>
2022-11-02 21:36:49 +0900

    chore: ログインボタン設置

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,13 +1,22 @@
 import { memo, FC } from 'react';
-import { Box, Button, Divider, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
 
 export const Login: FC = memo(() => {
   return (
-    <Flex>
-      <Box>
-        <Heading as="h1">ログイン</Heading>
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          TransPong
+        </Heading>
         <Divider />
-        <Button>42ユーザー認証</Button>
+        <Stack spacing={4} py={4} px={10}>
+          <Button my={10} bg="teal.300" color="white" _hover={{ opacity: 0.8 }}>
+            42ユーザー認証
+          </Button>
+          <Button>アドミンテスト1</Button>
+          <Button>アドミンテスト2</Button>
+          <Button>アドミンテスト3</Button>
+        </Stack>
       </Box>
     </Flex>
   );
