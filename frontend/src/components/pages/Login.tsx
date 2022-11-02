9b7ca0b7a05cd65d088fe9a46c9179566c4ff8fc
yuki <shimazuyuki7@gmail.com>
2022-11-03 00:08:12 +0900

    fix: コンソールにてログインデータ取得(アクセストークは自前で取ってきて試せる)

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,18 +1,24 @@
 import { memo, FC } from 'react';
 import { Box, Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
 import axios from 'axios';
+import { User } from 'types/api/user';
 // import { PrimaryButton } from 'components/atoms/button/PrimaryButton';
 
 export const Login: FC = memo(() => {
   const API_URL = 'https://api.intra.42.fr/v2/me';
+  // ここのアクセストークンは現状手動で取ってくる
   const ACCESS_TOKEN =
     '012bf266174087b6318c2cf1526c914cf5d033b58183df72bb724ffdf5badfb7';
 
   const onClickAuth = () => {
     axios
-      .get(API_URL, { headers: { Authorization: 'Bearer ' + ACCESS_TOKEN } })
+      .get<User[]>(API_URL, {
+        headers: { Authorization: 'Bearer ' + ACCESS_TOKEN },
+      })
       .then((res) => {
         console.log(res.data);
+        // eslintに怒られるのでコメントアウト
+        // alert(`Hello ${res.data.login}`);
       })
       .catch((err) => {
         console.log('err: ', err);
