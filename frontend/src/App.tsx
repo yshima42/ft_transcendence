729c99e76c2a060306621e52713154b05d8e9bcf
yuki <shimazuyuki7@gmail.com>
2022-11-02 21:15:30 +0900

    chore: ヘッダーとメニュー追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import './App.css';
+// import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from 'router/Router';
@@ -8,7 +8,6 @@ import theme from './theme/theme';
 const App: FC = () => {
   return (
     <div className="App">
-      <h1>ft_transcendence</h1>
       <ChakraProvider theme={theme}>
         <BrowserRouter>
           <Router />
