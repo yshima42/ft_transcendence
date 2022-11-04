fa47594bb0a5692606c903fdcdfd1a86d9b105db
hyoshie_vm <dymn9.term@gmail.com>
2022-11-04 11:53:36 +0900

    feat: jsonplaceholderからユーザー一覧を取得

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,63 +1,62 @@
 import { memo, FC } from 'react';
 import { Box, Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
-import axios from 'axios';
-import { AccessToken } from 'types/api/accessToken';
-import { User } from 'types/api/user';
+import { NavLink } from 'react-router-dom';
+// import axios from 'axios';
+// import { AccessToken } from 'types/api/accessToken';
+// import { User } from 'types/api/user';
 // import { PrimaryButton } from 'components/atoms/button/PrimaryButton';
 
 export const Login: FC = memo(() => {
-  // ②に使うデータ
-  const CLIENT_ID =
-    '13048d1985df54479741344156321e0b0d101970ad8c152c72f3de8c508c00a2';
-  const CLIENT_SECRET = '';
-  const CODE =
-    '1ec9adee482186608f021b228241cc847b86a3fcd175075ebc3752c27a9559fd';
+  // // ②に使うデータ
+  // const CLIENT_ID =
+  //   '13048d1985df54479741344156321e0b0d101970ad8c152c72f3de8c508c00a2';
+  // const CLIENT_SECRET = '';
+  // const CODE =
+  //   '1ec9adee482186608f021b228241cc847b86a3fcd175075ebc3752c27a9559fd';
 
-  // ③に使うデータ
-  const API_URL = 'https://api.intra.42.fr/v2/me';
-  // ここのアクセストークンは現状手動で取ってくる
-  const ACCESS_TOKEN =
-    '012bf266174087b6318c2cf1526c914cf5d033b58183df72bb724ffdf5badfb7';
+  // // ③に使うデータ
+  // const API_URL = 'https://api.intra.42.fr/v2/me';
+  // // ここのアクセストークンは現状手動で取ってくる
+  // const ACCESS_TOKEN =
+  //   '012bf266174087b6318c2cf1526c914cf5d033b58183df72bb724ffdf5badfb7';
 
-  const onClickAuth = () => {
-    // ① codeを取得
-    // 未実装
-
-    // ② codeを使ってアクセストークンを取得
-    // エラー出るので一旦コメントアウト
-    const params = new FormData();
-    params.append('grant_type', 'authorization_code');
-    params.append('client_id', CLIENT_ID);
-    params.append('client_secret', CLIENT_SECRET);
-    params.append('code', CODE);
-    params.append('redirect_uri', 'https://42tokyo.jp');
-    axios
-      .post<AccessToken[]>('https://api.intra.42.fr/oauth/token', params, {
-        headers: {
-          'content-type': 'multipart/form-data',
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log('err: ', err);
-      });
-
-    // ③ アクセストークンを使ってloginIdを取ってくる
-    axios
-      .get<User[]>(API_URL, {
-        headers: { Authorization: 'Bearer ' + ACCESS_TOKEN },
-      })
-      .then((res) => {
-        console.log(res.data);
-        // eslintに怒られるのでコメントアウト
-        // alert(`Hello ${res.data.login}`);
-      })
-      .catch((err) => {
-        console.log('err: ', err);
-      });
-  };
+  // const onClickAuth = () => {
+  //   // ① codeを取得
+  //   // 未実装
+  //   // ② codeを使ってアクセストークンを取得
+  //   // エラー出るので一旦コメントアウト
+  //   const params = new FormData();
+  //   params.append('grant_type', 'authorization_code');
+  //   params.append('client_id', CLIENT_ID);
+  //   params.append('client_secret', CLIENT_SECRET);
+  //   params.append('code', CODE);
+  //   params.append('redirect_uri', 'https://42tokyo.jp');
+  //   axios
+  //     .post<AccessToken[]>('https://api.intra.42.fr/oauth/token', params, {
+  //       headers: {
+  //         'content-type': 'multipart/form-data',
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log('err: ', err);
+  //     });
+  //   // ③ アクセストークンを使ってloginIdを取ってくる
+  //   axios
+  //     .get<User[]>(API_URL, {
+  //       headers: { Authorization: 'Bearer ' + ACCESS_TOKEN },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       // eslintに怒られるのでコメントアウト
+  //       // alert(`Hello ${res.data.login}`);
+  //     })
+  //     .catch((err) => {
+  //       console.log('err: ', err);
+  //     });
+  // };
 
   return (
     <Flex align="center" justify="center" height="100vh">
@@ -67,7 +66,9 @@ export const Login: FC = memo(() => {
         </Heading>
         <Divider />
         <Stack spacing={4} py={4} px={10}>
-          <Button onClick={onClickAuth}>42ユーザー認証</Button>
+          <Button>
+            <NavLink to="/user-list">42ユーザー認証</NavLink>
+          </Button>
           <Button>アドミンテスト1</Button>
           <Button>アドミンテスト2</Button>
           <Button>アドミンテスト3</Button>
--- frontend/src/components/pages/UserList.tsx
+++ frontend/src/components/pages/UserList.tsx
@@ -1,44 +1,38 @@
-import { memo, FC } from 'react';
+import { memo, FC, useEffect } from 'react';
 import {
   Table,
   Tbody,
   Tfoot,
   Tr,
-  Th,
-  TableCaption,
+  Thead,
   TableContainer,
 } from '@chakra-ui/react';
 import { UserTableData } from 'components/organisms/user/UserTableData';
+import { useAllUsers } from '../../hooks/useAllUsers';
 
 export const UserList: FC = memo(() => {
+  const { getUsers, users } = useAllUsers();
+
+  useEffect(() => getUsers(), [getUsers]);
+
   return (
     <>
       <TableContainer>
         <Table variant="striped" colorScheme="teal">
-          <TableCaption>Imperial to metric conversion factors</TableCaption>
-          {/* <Thead>
-            <Tr>
-              <Th>Login</Th>
-              <Th>Photo</Th>
-              <Th></Th>
-            </Tr>
-          </Thead> */}
+          <Thead>
+            <Tr></Tr>
+          </Thead>
           <Tbody>
-            {[...Array<string>(10)].map((_, index) => (
-              <>
-                <UserTableData
-                  login={`hyoshie${index}`}
-                  photo={`hogehoge${index}.com`}
-                />
-              </>
+            {users.map((jsonUsers) => (
+              <UserTableData
+                key={jsonUsers.id}
+                login={jsonUsers.username}
+                photo={jsonUsers.website}
+              />
             ))}
           </Tbody>
           <Tfoot>
-            <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
-            </Tr>
+            <Tr></Tr>
           </Tfoot>
         </Table>
       </TableContainer>
