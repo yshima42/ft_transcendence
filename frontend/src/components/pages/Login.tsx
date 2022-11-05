346ca55f8f7accc615bdac4ae43fe9ed67bb7d22
yuki <shimazuyuki7@gmail.com>
2022-11-05 15:22:54 +0900

    chore: 認証によるページ遷移の制限にトライしたが終わらなかったので、コメントアウトにしてアップ
    Co-authored-by: Hiroki Yoshie <hyoshie@users.noreply.github.com>

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,63 +1,8 @@
 import { memo, FC } from 'react';
 import { Box, Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
-import { NavLink } from 'react-router-dom';
-// import axios from 'axios';
-// import { AccessToken } from 'types/api/accessToken';
-// import { User } from 'types/api/user';
-// import { PrimaryButton } from 'components/atoms/button/PrimaryButton';
+import { Link } from 'react-router-dom';
 
 export const Login: FC = memo(() => {
-  // // ②に使うデータ
-  // const CLIENT_ID =
-  //   '13048d1985df54479741344156321e0b0d101970ad8c152c72f3de8c508c00a2';
-  // const CLIENT_SECRET = '';
-  // const CODE =
-  //   '1ec9adee482186608f021b228241cc847b86a3fcd175075ebc3752c27a9559fd';
-
-  // // ③に使うデータ
-  // const API_URL = 'https://api.intra.42.fr/v2/me';
-  // // ここのアクセストークンは現状手動で取ってくる
-  // const ACCESS_TOKEN =
-  //   '012bf266174087b6318c2cf1526c914cf5d033b58183df72bb724ffdf5badfb7';
-
-  // const onClickAuth = () => {
-  //   // ① codeを取得
-  //   // 未実装
-  //   // ② codeを使ってアクセストークンを取得
-  //   // エラー出るので一旦コメントアウト
-  //   const params = new FormData();
-  //   params.append('grant_type', 'authorization_code');
-  //   params.append('client_id', CLIENT_ID);
-  //   params.append('client_secret', CLIENT_SECRET);
-  //   params.append('code', CODE);
-  //   params.append('redirect_uri', 'https://42tokyo.jp');
-  //   axios
-  //     .post<AccessToken[]>('https://api.intra.42.fr/oauth/token', params, {
-  //       headers: {
-  //         'content-type': 'multipart/form-data',
-  //       },
-  //     })
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log('err: ', err);
-  //     });
-  //   // ③ アクセストークンを使ってloginIdを取ってくる
-  //   axios
-  //     .get<User[]>(API_URL, {
-  //       headers: { Authorization: 'Bearer ' + ACCESS_TOKEN },
-  //     })
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       // eslintに怒られるのでコメントアウト
-  //       // alert(`Hello ${res.data.login}`);
-  //     })
-  //     .catch((err) => {
-  //       console.log('err: ', err);
-  //     });
-  // };
-
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -67,7 +12,7 @@ export const Login: FC = memo(() => {
         <Divider />
         <Stack spacing={4} py={4} px={10}>
           <Button>
-            <NavLink to="/user-list">42ユーザー認証</NavLink>
+            <Link to="/user-list">42ユーザー認証</Link>
           </Button>
           <Button>アドミンテスト1</Button>
           <Button>アドミンテスト2</Button>
--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -8,6 +8,73 @@ import { HeaderLayout } from 'components/templates/HeaderLayout';
 import { Login } from '../components/pages/Login';
 import { Page404 } from '../components/pages/Page404';
 
+// type AuthContextType = {
+//   user: string;
+//   signin: (user: string, callback: VoidFunction) => void;
+//   signout: (callback: VoidFunction) => void;
+// };
+
+// // あまり良くない書き方とのことなので、後ほど検討(参考：https://qiita.com/johnmackay150/items/88654e5064290c24a32a)
+// // const AuthContext = createContext<AuthContextType | undefined>(undefined);
+// const AuthContext = createContext<AuthContextType>({
+//   user: '',
+//   signin: (newUser: string, callback: VoidFunction) => {
+//     callback();
+//   },
+//   signout: (callback: VoidFunction) => {
+//     callback();
+//   },
+// });
+
+// const fakeAuthProvider = {
+//   isAuthenticated: false,
+//   signin(callback: VoidFunction) {
+//     fakeAuthProvider.isAuthenticated = true;
+//     setTimeout(callback, 100); // fake async
+//   },
+//   signout(callback: VoidFunction) {
+//     fakeAuthProvider.isAuthenticated = false;
+//     setTimeout(callback, 100);
+//   },
+// };
+
+// const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+//   const [user, setUser] = useState('');
+
+//   const signin = (newUser: string, callback: VoidFunction) => {
+//     return fakeAuthProvider.signin(() => {
+//       setUser(newUser);
+//       callback();
+//     });
+//   };
+
+//   const signout = (callback: VoidFunction) => {
+//     return fakeAuthProvider.signout(() => {
+//       setUser('');
+//       callback();
+//     });
+//   };
+
+//   const value = { user, signin, signout };
+
+//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+// };
+
+// const useAuth = () => {
+//   return useContext(AuthContext);
+// };
+
+// const RequireAuth = ({ children }: { children: JSX.Element }) => {
+//   const auth = useAuth();
+//   const location = useLocation();
+
+//   if (auth.user === '') {
+//     return <Navigate to="/" state={{ from: location }} replace />;
+//   }
+
+//   return children;
+// };
+
 export const Router = (): React.ReactElement | null => {
   return (
     <Routes>
