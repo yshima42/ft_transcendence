1c5eb790ecd4d30d78eb877f359f9641c4216ca9
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-06 15:34:06 +0900

    UserController実装中。

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { UserService } from './user.service';
 
+// @UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): User {
+    return req.user;
+  }
+}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UserController],
   providers: [UserService],
 })
