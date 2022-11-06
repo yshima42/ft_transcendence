b93756e79a49d120258957754bf9c18b53dc627e
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-06 13:28:21 +0900

    Dtoの修正と、jwtをそのまま返すように修正。

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -9,32 +9,21 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto/auth.dto';
+import { CreateUserDto } from './dto/createUserDto.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signUp(@Body() dto: AuthDto): Promise<{ message: string }> {
+  async signUp(@Body() dto: CreateUserDto): Promise<{ message: string }> {
     return await this.authService.signUp(dto);
   }
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  async login(
-    @Body() dto: AuthDto,
-    @Res({ passthrough: true }) res: Response
-  ): Promise<{ message: string }> {
-    const jwt = await this.authService.login(dto);
-    res.cookie('access_token', jwt.accessToken, {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'none',
-      path: '/',
-    });
-
-    return { message: 'ok' };
+  async login(@Body() dto: CreateUserDto): Promise<{ accessToken: string }> {
+    return await this.authService.login(dto);
   }
 
   @HttpCode(HttpStatus.OK)
