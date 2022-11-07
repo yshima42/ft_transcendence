2fcbfd0cc6fc0b2e79262e3be62921027ad5ba69
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-07 11:22:01 +0900

    eslint修正。

--- backend/src/auth/decorator/get-user.decorator.ts
+++ backend/src/auth/decorator/get-user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator((_, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest(); // eslint-disable-line
+  const request: { user: string } = ctx.switchToHttp().getRequest();
 
-  return request.user; // eslint-disable-line
+  return request.user;
 });
