86525f6fe5ca3f6bb14107d34fe6135ce041dc13
mkamei <mkamei@student.42tokyo.jp>
2022-11-07 16:34:36 +0900

    get-user デコレーターを修正

--- backend/src/auth/decorator/get-user.decorator.ts
+++ backend/src/auth/decorator/get-user.decorator.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 export const GetUser = createParamDecorator((_, ctx: ExecutionContext) => {
-  const request: { user: string } = ctx.switchToHttp().getRequest();
+  const request: { user: { user: User } } = ctx.switchToHttp().getRequest();
 
-  return request.user;
+  return request.user.user;
 });
