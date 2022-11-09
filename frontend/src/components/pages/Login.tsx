7f73e521ccf23052cf4f832442bcaadfa3929e56
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-09 17:04:46 +0900

    chore: AccessTokenProviderを追加。
    Co-authored-by: yshima <yshima42@users.noreply.github.com>

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,16 +1,34 @@
-import { memo, FC } from 'react';
+import { memo, FC, useContext } from 'react';
 import { Box, Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
 import axios from 'axios';
-import { Outlet } from 'react-router-dom';
+import { AccessTokenContext } from 'hooks/providers/useAccessTokenProvider';
+import { useAuth } from 'hooks/providers/useAuthProvider';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 export const Login: FC = memo(() => {
+  const { accessToken, setAccessToken } = useContext(AccessTokenContext);
+
+  const navigate = useNavigate();
+  const auth = useAuth();
+
+  const to = '/user-list';
+
   const onClickDummy1 = () => {
     const params = new URLSearchParams();
     params.append('name', 'dummy1');
     axios
       .post('http://localhost:3000/auth/login/dummy', params)
       .then((result) => {
-        console.log(result);
+        // const at: string = result.data as string;
+        // console.log(at);
+        // console.log(result.data as string);
+
+        setAccessToken(result.data as string);
+        console.log(accessToken);
+
+        auth.signin('dummy1', () => {
+          navigate(to, { replace: true });
+        });
       })
       .catch(() => console.log('error'));
   };
