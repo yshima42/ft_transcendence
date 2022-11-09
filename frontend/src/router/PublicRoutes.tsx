4e92bef4cfaf28fb76731bfe3213caf43f67e10d
yuki <shimazuyuki7@gmail.com>
2022-11-09 21:57:24 +0900

    chore: routerをbulletproof-react参考にリファクタリング

--- frontend/src/router/PublicRoutes.tsx
+++ frontend/src/router/PublicRoutes.tsx
@@ -1,9 +1,8 @@
-import { FC } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../hooks/providers/useAuthProvider';
+import { Login } from 'components/pages/Login';
 
-export const PublicRoutes: FC = () => {
-  const auth = useAuth();
-
-  return auth.user !== '' ? <Navigate to="/user-list" /> : <Outlet />;
-};
+export const publicRoutes = [
+  {
+    path: '/',
+    element: <Login />,
+  },
+];
