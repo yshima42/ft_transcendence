31cae13491ab66dff54d8e26cb1cc80c1a0d2b73
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-11 14:25:26 +0900

    feat: ログイン機能を追加しました。

--- frontend/src/components/molecules/AuthStatus.tsx
+++ frontend/src/components/molecules/AuthStatus.tsx
@@ -1,25 +1,61 @@
-import { FC, memo } from 'react';
-import { useAuth } from 'hooks/providers/useAuthProvider';
+import { FC, memo, useState } from 'react';
+import { Box, Icon } from '@chakra-ui/react';
+import axios from 'axios';
+import { GrLogout } from 'react-icons/gr';
 import { useNavigate } from 'react-router-dom';
+import { User } from 'types/api/user';
 
 export const AuthStatus: FC = memo(() => {
-  const auth = useAuth();
+  const [me, setMe] = useState('');
   const navigate = useNavigate();
 
-  if (auth.user === '') {
-    return <p>You are not logged in.</p>;
-  }
+  const onClickLogout = () => {
+    const params = new URLSearchParams();
+    axios
+      .post('http://localhost:3000/auth/logout', params, {
+        withCredentials: true,
+      })
+      .then(() => navigate('/', { replace: true }))
+      .catch(() => alert('error'));
+  };
+
+  axios
+    .get<User>('http://localhost:3000/user/me', { withCredentials: true })
+    .then((res) => {
+      setMe(res.data.name);
+    })
+    .catch(() => navigate('/', { replace: true }));
 
   return (
-    <p>
-      Hello {auth.user}!{' '}
-      <button
-        onClick={() => {
-          auth.signout(() => navigate('/'));
-        }}
+    <>
+      <Box px={4}>{me}</Box>
+      <Icon
+        as={GrLogout}
+        onClick={onClickLogout}
+        cursor="pointer"
+        _hover={{ opacity: 0.8 }}
       >
-        Sign out
-      </button>
-    </p>
+        logout
+      </Icon>
+    </>
   );
+  // const auth = useAuth();
+  // const navigate = useNavigate();
+
+  // if (auth.user === '') {
+  //   return <p>You are not logged in.</p>;
+  // }
+
+  // return (
+  //   <p>
+  //     Hello {auth.user}!{' '}
+  //     <button
+  //       onClick={() => {
+  //         auth.signout(() => navigate('/'));
+  //       }}
+  //     >
+  //       Sign out
+  //     </button>
+  //   </p>
+  // );
 });
