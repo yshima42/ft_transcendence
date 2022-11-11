ee04ce5f0188d1ff42f030700903114606d9681f
Masaya Kamei <km9smc6@gmail.com>
2022-11-11 18:23:37 +0900

    Refactoring auth

--- backend/src/auth/strategy/ft.strategy.ts
+++ backend/src/auth/strategy/ft.strategy.ts
@@ -9,7 +9,7 @@ export class FtStrategy extends PassportStrategy(Strategy, '42') {
     super({
       clientID: config.get<string>('FT_CID'),
       clientSecret: config.get<string>('FT_SECRET'),
-      callbackURL: 'http://localhost:3000/auth/login/42/return',
+      callbackURL: 'http://localhost:3000/auth/login/42/callback',
     });
   }
 
