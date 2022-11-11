1154cb878bc98bcd127d0fa931f50badb5beb6e5
yuki <shimazuyuki7@gmail.com>
2022-11-11 11:21:34 +0900

    feat: フロント側でのAuth制限の削除

--- frontend/src/hooks/useAllUsers.ts
+++ frontend/src/hooks/useAllUsers.ts
@@ -1,20 +1,17 @@
-import { useCallback, useContext, useState } from 'react';
+import { useCallback, useState } from 'react';
 import axios from 'axios';
 
 import { User } from 'types/api/user';
-import { AccessTokenContext } from './providers/useAccessTokenProvider';
 
 export const useAllUsers = (): {
   getUsers: () => void;
   users: User[];
 } => {
   const [users, setUsers] = useState<User[]>([]);
-  const { token } = useContext(AccessTokenContext);
 
   const getUsers = useCallback(() => {
-    axios.defaults.headers.common.Authorization = 'Bearer ' + token;
     axios
-      .get<User[]>('http://localhost:3000/user/all')
+      .get<User[]>('http://localhost:3000/user/all', { withCredentials: true })
       .then((res) => setUsers(res.data))
       .catch(() => alert('error'));
   }, []);
