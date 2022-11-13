6282ef3fca7235a8d24a3c01199c0c9c49cf4175
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-14 00:20:07 +0900

    refactor: インスタンス化したaxiosを使うように修正。

--- frontend/src/hooks/useMe.tsx
+++ frontend/src/hooks/useMe.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-import axios from 'axios';
 import { User } from 'features/users/types/user';
 import { useNavigate } from 'react-router-dom';
+
+import { axios } from '../lib/axios';
 
 export const useMe = (): string => {
   const [me, setMe] = useState('');
   const navigate = useNavigate();
 
   axios
-    .get<User>('http://localhost:3000/user/me', { withCredentials: true })
+    .get<User>('/user/me')
     .then((res) => {
       setMe(res.data.name);
     })
