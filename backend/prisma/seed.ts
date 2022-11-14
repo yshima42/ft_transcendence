db54b88210a35966ea225d3549dd42915b7a3d28
Masaya Kamei <km9smc6@gmail.com>
2022-11-14 12:18:30 +0900

    db にavatarPath を追加

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -7,30 +7,35 @@ const userData: User[] = [
     createdAt: new Date('2022-11-01T04:34:22+09:00'),
     updatedAt: new Date('2022-11-01T04:34:22+09:00'),
     name: 'dummy1',
+    avatarPath: null,
   },
   {
     id: '40e8b4b4-9b39-4b7e-8e31-78e31975d320',
     createdAt: new Date('2022-11-01T04:34:22+09:00'),
     updatedAt: new Date('2022-11-01T04:34:22+09:00'),
     name: 'dummy2',
+    avatarPath: null,
   },
   {
     id: '5001da8b-0316-411e-a34f-1db29d4d4c4b',
     createdAt: new Date('2022-11-01T04:34:22+09:00'),
     updatedAt: new Date('2022-11-01T04:34:22+09:00'),
     name: 'dummy3',
+    avatarPath: null,
   },
   {
     id: '7fd8fa2a-398f-495a-bb55-7290136c7e3f',
     createdAt: new Date('2022-11-01T04:34:22+09:00'),
     updatedAt: new Date('2022-11-01T04:34:22+09:00'),
     name: 'dummy4',
+    avatarPath: null,
   },
   {
     id: '9f1b53bf-e25d-4630-a174-ac4c7adadcd6',
     createdAt: new Date('2022-11-01T04:34:22+09:00'),
     updatedAt: new Date('2022-11-01T04:34:22+09:00'),
     name: 'dummy5',
+    avatarPath: null,
   },
 ];
 
