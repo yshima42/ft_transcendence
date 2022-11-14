b1364bb2cc928c5a4d153c086b2ddfbcefcbdd9d
hyoshie_vm <dymn9.term@gmail.com>
2022-11-14 12:54:08 +0900

    feat: friendのapiを簡易実装

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator/get-user.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
@@ -19,4 +19,54 @@ export class UsersController {
   findMe(@GetUser() user: User): User {
     return user;
   }
+
+  @Post(':id/friends')
+  @UseGuards(JwtAuthGuard)
+  async sendFriendRequest(
+    @Param('id') id: string,
+    @GetUser() user: User
+  ): Promise<User> {
+    return await this.usersService.sendFriendRequest(id, user);
+  }
+
+  @Get(':id/following')
+  @UseGuards(JwtAuthGuard)
+  async findFollowing(@Param('id') id: string): Promise<User[]> {
+    return await this.usersService.findFollowing(id);
+  }
+
+  @Get(':id/followedBy')
+  @UseGuards(JwtAuthGuard)
+  async findFollowedBy(@Param('id') id: string): Promise<User[]> {
+    return await this.usersService.findFollowedBy(id);
+  }
+
+  @Get(':id/friends')
+  @UseGuards(JwtAuthGuard)
+  async findFriends(@Param('id') id: string): Promise<User[]> {
+    return await this.usersService.findFriends(id);
+  }
+
+  // @Post(':id/friends')
+  // @UseGuards(JwtAuthGuard)
+  // sendFriendRequest(@Param('id') id: string, @GetUser() user: User): string {
+  //   console.log(id);
+  //   console.log(user);
+
+  //   return `${user.name} follows ${id}`;
+  // }
+
+  // @Get(':id')
+  // @UseGuards(JwtAuthGuard)
+  // displayId(@Param('id') id: string): string {
+  //   return this.usersService.displayId(id);
+  // }
+
+  // @Get('follow/:id')
+  // @UseGuards(JwtAuthGuard)
+  // displayFollowId(@Param('id') id: string): string {
+  //   console.log(`follow ${id}`);
+
+  //   return `followed`;
+  // }
 }
