349cf81c9774eed7eccf313c2dadcdd0056360cc
yuki <shimazuyuki7@gmail.com>
2022-11-14 21:21:31 +0900

    chore: figmaで決めたルーティングの整理とモック追加

---
+++ frontend/src/features/chat/api/useAllChats.ts
@@ -0,0 +1,62 @@
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { axios } from '../../../lib/axios';
+import { Chat } from '../types/chat';
+
+const mockChats: Chat[] = [
+  {
+    createdAt: '11-14',
+    id: '1',
+    members: 'dummy1,dummy2,dummy3',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '2',
+    members: 'dummy1,dummy2,dummy3',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '3',
+    members: 'dummy1,dummy2,dummy3',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '4',
+    members: 'dummy1,dummy2,dummy3',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '5',
+    members: 'dummy1,dummy2,dummy3',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '6',
+    members: 'dummy1,dummy2,dummy3',
+    updatedAt: '11-14',
+  },
+];
+
+export const useAllChats = (): {
+  getChats: () => void;
+  chats: Chat[];
+} => {
+  const [chats, setChats] = useState<Chat[]>([]);
+  const navigate = useNavigate();
+
+  const getChats = useCallback(() => {
+    // ここをChatsゲットしてきてmockChatsを入れ替える
+    axios
+      .get<Chat[]>('/user/all')
+      .then(() => setChats(mockChats))
+      .catch(() => navigate('/', { replace: true }));
+  }, [navigate]);
+
+  return { getChats, chats };
+};
---
+++ frontend/src/features/game/api/useAllGames.ts
@@ -0,0 +1,68 @@
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { axios } from '../../../lib/axios';
+import { Game } from '../types/game';
+
+const mockGames: Game[] = [
+  {
+    createdAt: '11-14',
+    id: '1',
+    player1: 'dummy1',
+    player2: 'dummy2',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '2',
+    player1: 'dummy1',
+    player2: 'dummy2',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '3',
+    player1: 'dummy1',
+    player2: 'dummy2',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '4',
+    player1: 'dummy1',
+    player2: 'dummy2',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '5',
+    player1: 'dummy1',
+    player2: 'dummy2',
+    updatedAt: '11-14',
+  },
+  {
+    createdAt: '11-14',
+    id: '6',
+    player1: 'dummy1',
+    player2: 'dummy2',
+    updatedAt: '11-14',
+  },
+];
+
+export const useAllGames = (): {
+  getGames: () => void;
+  games: Game[];
+} => {
+  const [games, setGames] = useState<Game[]>([]);
+  const navigate = useNavigate();
+
+  const getGames = useCallback(() => {
+    // ここをGamesゲットしてきてmockGamesを入れ替える
+    axios
+      .get<Game[]>('/user/all')
+      .then(() => setGames(mockGames))
+      .catch(() => navigate('/', { replace: true }));
+  }, [navigate]);
+
+  return { getGames, games };
+};
