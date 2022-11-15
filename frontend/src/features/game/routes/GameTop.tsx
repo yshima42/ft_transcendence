725a650556025c1a36a2e334f93a24e906b43bb4
yuki <shimazuyuki7@gmail.com>
2022-11-15 13:51:44 +0900

    chore: マッチングデザインの適応

--- frontend/src/features/game/routes/GameTop.tsx
+++ frontend/src/features/game/routes/GameTop.tsx
@@ -1,18 +1,18 @@
 import { memo, FC } from 'react';
-import { Box, Button, Stack } from '@chakra-ui/react';
+import { Box, Button, Center, Stack } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 export const GameTop: FC = memo(() => {
   return (
-    <>
+    <Center>
       <Stack spacing={4} py={4} px={10}>
         <Box w="100%" bg="gray.200">
-          個人統計情報等を入れる
+          個人統計情報等
         </Box>
         <Link to="matching">
           <Button>ランクマッチ</Button>
         </Link>
       </Stack>
-    </>
+    </Center>
   );
 });
--- frontend/src/features/game/routes/Matching.tsx
+++ frontend/src/features/game/routes/Matching.tsx
@@ -1,15 +1,44 @@
-import { memo, FC } from 'react';
-import { Button } from '@chakra-ui/react';
-import { Link, Outlet } from 'react-router-dom';
+import { memo, FC, useState } from 'react';
+import { Box, Divider, Flex, Heading, Spinner, Stack } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { PrimaryButton } from 'components/atoms/button/PrimaryButton';
 
 export const Matching: FC = memo(() => {
+  const navigate = useNavigate();
+  const [matched, setMatched] = useState(false);
+
+  const onClickMatch = () => {
+    setMatched(true);
+    setTimeout(() => navigate('/app/game'), 3 * 1000);
+  };
+
+  const onClickCancel = () => {
+    navigate(-1);
+  };
+
   return (
     <>
-      <p>マッチング画面です</p>
-      <Link to="starting">
-        <Button>マッチングしたと仮定するボタン</Button>
-      </Link>
-      <Outlet />
+      <Flex align="center" justify="center" height="40vh">
+        <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+          <Heading as="h1" size="lg" textAlign="center">
+            マッチング中
+          </Heading>
+          <Divider />
+          <Stack spacing={4} py={4} px={10} align="center">
+            {matched ? <Box>3秒で始まります</Box> : <Spinner />}
+            <PrimaryButton
+              onClick={onClickMatch}
+              loading={matched}
+              disabled={matched}
+            >
+              マッチングしたと仮定する
+            </PrimaryButton>
+            <PrimaryButton onClick={onClickCancel} disabled={matched}>
+              キャンセル
+            </PrimaryButton>
+          </Stack>
+        </Box>
+      </Flex>
     </>
   );
 });
