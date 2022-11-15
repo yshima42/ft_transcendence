e91313debaed958edc93c0963b0ab68195b65fda
hyoshie_vm <dymn9.term@gmail.com>
2022-11-15 09:14:49 +0900

    refactor: Relationshipのテーブルを追加

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Relationship, User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
-
-// 場所あとで変える
-type FollowRelation = {
-  following: User[];
-  followedBy: User[];
-};
 
 @Injectable()
 export class UsersService {
@@ -20,102 +14,90 @@ export class UsersService {
     return users;
   }
 
-  async sendFriendRequest(targetId: string, requester: User): Promise<User> {
-    const updateUser = await this.prisma.user.update({
-      where: { id: requester.id },
-      data: {
-        updatedAt: new Date(),
-        following: {
-          connect: [{ id: targetId }],
+  // あとでリファクタ
+  // 登録済みのものをもう一度createしようとすると例外投げている
+  // 自分との関係作れてそう
+  async createRelationship(
+    userId: string,
+    targetUserId: string
+  ): Promise<Relationship> {
+    const isFollowdBy = await this.prisma.relationship.findUnique({
+      where: {
+        userId_targetUserId: {
+          userId: targetUserId,
+          targetUserId: userId,
         },
       },
     });
 
-    return updateUser;
-  }
+    const isFriends = isFollowdBy != null;
+    if (isFriends) {
+      await this.prisma.relationship.update({
+        where: {
+          userId_targetUserId: {
+            userId: targetUserId,
+            targetUserId: userId,
+          },
+        },
+        data: {
+          isFriends: true,
+        },
+      });
+    }
 
-  // followingが存在しないとき、空の配列を返している
-  // null返してもいいならそれが楽
-  // 今のreturn文の書き方違和感あるのであとで誰かに聞く
-  // async findFollowing(id: string): Promise<User[]> {
-  //   const users = await this.prisma.user.findUnique({
-  //     where: { id },
-  //     select: {
-  //       following: true,
-  //     },
-  //   });
-
-  //   return users != null ? users.following : [];
-  // }
-
-  async findFollowing(id: string): Promise<User[]> {
-    const { following, followedBy } = await this.findFollowRelation(id);
-    const idArrayIntersect = this.getIdArraysIntersect(following, followedBy);
-
-    const targetUsers = [...following].filter((val) => {
-      return !idArrayIntersect.includes(val.id);
-    });
-    console.log(targetUsers);
-
-    return targetUsers;
-  }
-
-  async findFollowedBy(id: string): Promise<User[]> {
-    const { following, followedBy } = await this.findFollowRelation(id);
-    const idArrayIntersect = this.getIdArraysIntersect(following, followedBy);
-    const targetUsers = [...followedBy].filter((val) => {
-      return !idArrayIntersect.includes(val.id);
-    });
-
-    return targetUsers;
-  }
-
-  // findFollowingと同じく戻り値に違和感
-  // async findFollowedBy(id: string): Promise<User[]> {
-  //   const users = await this.prisma.user.findUnique({
-  //     where: { id },
-  //     select: {
-  //       followedBy: true,
-  //     },
-  //   });
-
-  //   return users != null ? users.followedBy : [];
-  // }
-
-  getIdArraysIntersect = (following: User[], followedBy: User[]): string[] => {
-    const followingIdArray = following.map((itm) => {
-      return itm.id;
-    });
-    const followedByIdArray = followedBy.map((itm) => {
-      return itm.id;
-    });
-    const idArrayIntersect = [...followingIdArray].filter((val) =>
-      followedByIdArray.includes(val)
-    );
-
-    return idArrayIntersect;
-  };
-
-  async findFollowRelation(id: string): Promise<FollowRelation> {
-    const user = await this.prisma.user.findUnique({
-      where: { id },
-      select: {
-        following: true,
-        followedBy: true,
+    return await this.prisma.relationship.create({
+      data: {
+        userId,
+        targetUserId,
+        isFriends,
       },
     });
-    const following = user !== null ? user.following : [];
-    const followedBy = user !== null ? user.followedBy : [];
-
-    return { following, followedBy };
   }
 
-  async findFriends(id: string): Promise<User[]> {
-    const { following, followedBy } = await this.findFollowRelation(id);
-    const idArrayIntersect = this.getIdArraysIntersect(following, followedBy);
-    const friends = [...following].filter((val) => {
-      return idArrayIntersect.includes(val.id);
+  async findFollowing(userId: string): Promise<User[]> {
+    const users = await this.prisma.relationship.findMany({
+      where: { userId, isFriends: false },
+      select: {
+        targetUser: true,
+      },
     });
+
+    const following: User[] = [];
+    for (const user of users) {
+      following.push(user.targetUser);
+    }
+
+    return following;
+  }
+
+  async findFollowedBy(userId: string): Promise<User[]> {
+    const users = await this.prisma.relationship.findMany({
+      where: { targetUserId: userId, isFriends: false },
+      select: {
+        user: true,
+      },
+    });
+
+    const followedBy: User[] = [];
+    for (const user of users) {
+      followedBy.push(user.user);
+    }
+
+    return followedBy;
+  }
+
+  async findFriends(userId: string): Promise<User[]> {
+    const users = await this.prisma.relationship.findMany({
+      where: { userId, isFriends: true },
+      select: {
+        targetUser: true,
+      },
+    });
+
+    const friends: User[] = [];
+    for (const user of users) {
+      friends.push(user.targetUser);
+    }
 
     return friends;
   }
