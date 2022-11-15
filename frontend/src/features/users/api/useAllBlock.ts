ba9698969b2e0555a86e47f632281a7e097ef88d
yuki <shimazuyuki7@gmail.com>
2022-11-15 10:51:27 +0900

    feat: タブでフレンド、ユーザー、ブロックと切り替えれるように実装

---
+++ frontend/src/features/users/api/useAllBlock.ts
@@ -0,0 +1,23 @@
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { axios } from '../../../lib/axios';
+import { User } from '../types/user';
+
+export const useAllBlock = (): {
+  getBlock: () => void;
+  block: User[];
+} => {
+  const [block, setBlock] = useState<User[]>([]);
+  const navigate = useNavigate();
+
+  const getBlock = useCallback(() => {
+    // ここをfriend取得のAPIに変える
+    axios
+      .get<User[]>('/user/all')
+      .then((res) => setBlock(res.data))
+      .catch(() => navigate('/', { replace: true }));
+  }, [navigate]);
+
+  return { getBlock, block };
+};
