8a725dd619e33f279b8c1e10df91bcdac255a7d8
hyoshie_vm <dymn9.term@gmail.com>
2022-11-16 15:21:09 +0900

    chore: migrationファイルを整理

--- backend/prisma/migrations/20221116061626_relationship/migration.sql
+++ backend/prisma/migrations/20221116061626_relationship/migration.sql
@@ -1,18 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `_UserFollows` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "_UserFollows" DROP CONSTRAINT "_UserFollows_A_fkey";
-
--- DropForeignKey
-ALTER TABLE "_UserFollows" DROP CONSTRAINT "_UserFollows_B_fkey";
-
--- DropTable
-DROP TABLE "_UserFollows";
-
 -- CreateTable
 CREATE TABLE "Relationship" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
