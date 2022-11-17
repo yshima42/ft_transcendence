aefd4773e2ae6001b7914981979fe370ad8a7679
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-17 09:42:48 +0900

    refactor: リファクタリングしました。

--- frontend/src/features/users/components/AvatarWithUploadButton.tsx
+++ frontend/src/features/users/components/AvatarWithUploadButton.tsx
@@ -1,6 +1,9 @@
-import { memo, FC } from 'react';
+import { memo, FC, useRef } from 'react';
 import { Image, Stack } from '@chakra-ui/react';
+import { User } from '@prisma/client';
+import { AxiosError } from 'axios';
 import { PrimaryButton } from 'components/atoms/button/PrimaryButton';
+import { axios } from '../../../lib/axios';
 
 type Props = {
   name: string;
@@ -10,7 +13,44 @@ type Props = {
 export const AvatarWithUploadButton: FC<Props> = memo((props) => {
   const { name, avatarUrl } = props;
 
-  const onClickChangeAvatar = () => alert('change avatar');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onClickUpload = () => {
+    console.log(inputRef.current);
+    inputRef.current?.click();
+  };
+
+  const onFileInputChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.currentTarget.files === null) {
+      return;
+    }
+    console.log(event.currentTarget.files[0]);
+
+    const file = event.currentTarget.files[0];
+
+    if (file === null) {
+      return;
+    }
+
+    let id = '';
+    await axios.get<User>('/users/me').then((res) => {
+      id = res.data.id;
+    });
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    await axios
+      .post(`/users/${id}/avatar`, formData)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e: AxiosError) => {
+        console.error(e);
+      });
+  };
 
   return (
     <Stack align="center" w={100} m={4}>
@@ -21,7 +61,14 @@ export const AvatarWithUploadButton: FC<Props> = memo((props) => {
         alt={name}
         m={4}
       />
-      <PrimaryButton onClick={onClickChangeAvatar}>UPLOAD</PrimaryButton>
+      <PrimaryButton onClick={onClickUpload}>UPLOAD</PrimaryButton>
+      <input
+        hidden
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+        onChange={onFileInputChange}
+      />
     </Stack>
   );
 });
--- frontend/src/features/users/routes/Profile.tsx
+++ frontend/src/features/users/routes/Profile.tsx
@@ -1,66 +1,10 @@
-import React, { memo, FC, useState } from 'react';
+import { memo, FC } from 'react';
 import { Flex } from '@chakra-ui/react';
-import { User } from '@prisma/client';
-import { AxiosError } from 'axios';
 import { ContentLayout } from 'components/templates/ContentLayout';
-import { axios } from '../../../lib/axios';
 import { AvatarWithUploadButton } from '../components/AvatarWithUploadButton';
 import { ProfileCard } from '../components/ProfileCard';
 
 export const Profile: FC = memo(() => {
-  const [file, setFile] = useState<File | null>(null);
-
-  const onChangeFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (files?.[0] != null) {
-      setFile(files[0]);
-    }
-
-    if (file === null) {
-      return;
-    }
-
-    let id = '';
-    await axios.get<User>('/users/me').then((res) => {
-      id = res.data.id;
-    });
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    await axios
-      .post(`/users/${id}/avatar`, formData)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((e: AxiosError) => {
-        console.error(e);
-      });
-  };
-
-  const onClickSubmit = async () => {
-    if (file === null) {
-      return;
-    }
-
-    let id = '';
-    await axios.get<User>('/users/me').then((res) => {
-      id = res.data.id;
-    });
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    await axios
-      .post(`/users/${id}/avatar`, formData)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((e: AxiosError) => {
-        console.error(e);
-      });
-  };
-
   return (
     <ContentLayout title="Profile">
       <Flex justify="center" padding={{ base: 5, md: 7 }}>
@@ -70,24 +14,6 @@ export const Profile: FC = memo(() => {
         />
         <ProfileCard name="yuuyuu" nickname="hakusho" />
       </Flex>
-
-      <div className="App">
-        <div className="App-form">
-          <input
-            name="file"
-            type="file"
-            accept="image/*"
-            onChange={onChangeFile}
-          />
-
-          <input
-            type="button"
-            disabled={file == null}
-            value="送信"
-            onClick={onClickSubmit}
-          />
-        </div>
-      </div>
     </ContentLayout>
   );
 });
