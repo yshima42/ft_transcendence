53c301293d4e2d293eda6d486efaf76407d493b0
yuki <shimazuyuki7@gmail.com>
2022-11-17 13:48:05 +0900

    refactor: usersコンポーネント分割

--- frontend/src/features/users/routes/Users.tsx
+++ frontend/src/features/users/routes/Users.tsx
@@ -1,52 +1,16 @@
-import { memo, FC, useState } from 'react';
+import { memo, FC } from 'react';
 
-import { Button, Center, Flex } from '@chakra-ui/react';
-import { Link, Outlet } from 'react-router-dom';
+import { Center } from '@chakra-ui/react';
+import { Outlet } from 'react-router-dom';
 import { ContentLayout } from 'components/templates/ContentLayout';
+import { UsersTopTab } from '../components/UsersTopTab';
 
 export const Users: FC = memo(() => {
-  const [active, setActive] = useState('Friends');
-
   return (
     <>
       <ContentLayout title="Users">
         <Center>
-          <Flex>
-            <Link to="">
-              <Button
-                size="sm"
-                onClick={() => setActive('Friends')}
-                bg={active === 'Friends' ? 'gray.300' : 'gray:100'}
-                borderRadius="5px"
-              >
-                Friends
-              </Button>
-            </Link>
-          </Flex>
-          <Flex>
-            <Link to="all">
-              <Button
-                size="sm"
-                onClick={() => setActive('Users')}
-                bg={active === 'Users' ? 'gray.300' : 'gray:100'}
-                borderRadius="5px"
-              >
-                Users
-              </Button>
-            </Link>
-          </Flex>
-          <Flex>
-            <Link to="block">
-              <Button
-                size="sm"
-                onClick={() => setActive('Block')}
-                bg={active === 'Block' ? 'gray.300' : 'gray:100'}
-                borderRadius="5px"
-              >
-                Block
-              </Button>
-            </Link>
-          </Flex>
+          <UsersTopTab />
         </Center>
         <Outlet />
       </ContentLayout>
