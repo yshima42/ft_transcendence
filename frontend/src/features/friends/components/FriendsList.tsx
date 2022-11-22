d83dc71f710cc9389c92f5be36b5080bc47a69b2
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-23 00:23:22 +0900

    表示をカード型に変更。

--- frontend/src/features/friends/components/FriendsList.tsx
+++ frontend/src/features/friends/components/FriendsList.tsx
@@ -1,42 +1,72 @@
 import { FC, memo, useEffect } from 'react';
-import { Avatar, AvatarBadge } from '@chakra-ui/react';
-import { User } from '@prisma/client';
-import { Link } from 'react-router-dom';
-import { PrimaryTable } from 'components/table/PrimaryTable';
+import { Wrap, WrapItem } from '@chakra-ui/react';
 import { useAllFriends } from '../hooks/useAllFriends';
-import { DirectMessageButton } from './DirectMessageButton';
+import { FriendCard } from './FriendCard';
 
 export const FriendsList: FC = memo(() => {
   const { getFriends, friends } = useAllFriends();
   useEffect(() => getFriends(), [getFriends]);
 
+  const onClickTest = () => {
+    alert('onClickTest');
+  };
+
   return (
-    <PrimaryTable<User>
-      data={friends}
-      columns={[
-        {
-          title: '',
-          Cell({ entry: { avatarUrl } }) {
-            return (
-              <Avatar src={avatarUrl}>
-                <AvatarBadge boxSize="1.1em" bg="green.500" />
-              </Avatar>
-            );
-          },
-        },
-        {
-          title: '',
-          Cell({ entry: { name } }) {
-            return <Link to={`./${name}`}>{name}</Link>;
-          },
-        },
-        {
-          title: '',
-          Cell({ entry: { name } }) {
-            return <DirectMessageButton id={name} />;
-          },
-        },
-      ]}
-    />
+    <Wrap p={{ base: 4, md: 10 }}>
+      {friends.map((obj) => (
+        <WrapItem key={obj.id} mx="auto">
+          <FriendCard
+            id={obj.id}
+            avatarUrl={obj.avatarUrl}
+            name={obj.name}
+            button="test"
+            onClick={onClickTest}
+          />
+        </WrapItem>
+      ))}
+    </Wrap>
   );
 });
+
+// import { FC, memo, useEffect } from 'react';
+// import { Avatar, AvatarBadge } from '@chakra-ui/react';
+// import { User } from '@prisma/client';
+// import { Link } from 'react-router-dom';
+// import { PrimaryTable } from 'components/table/PrimaryTable';
+// import { useAllFriends } from '../hooks/useAllFriends';
+// import { DirectMessageButton } from './DirectMessageButton';
+
+// export const FriendsList: FC = memo(() => {
+//   const { getFriends, friends } = useAllFriends();
+//   useEffect(() => getFriends(), [getFriends]);
+
+//   return (
+//     <PrimaryTable<User>
+//       data={friends}
+//       columns={[
+//         {
+//           title: '',
+//           Cell({ entry: { avatarUrl } }) {
+//             return (
+//               <Avatar src={avatarUrl}>
+//                 <AvatarBadge boxSize="1.1em" bg="green.500" />
+//               </Avatar>
+//             );
+//           },
+//         },
+//         {
+//           title: '',
+//           Cell({ entry: { name } }) {
+//             return <Link to={`./${name}`}>{name}</Link>;
+//           },
+//         },
+//         {
+//           title: '',
+//           Cell({ entry: { name } }) {
+//             return <DirectMessageButton id={name} />;
+//           },
+//         },
+//       ]}
+//     />
+//   );
+// });
--- frontend/src/features/friends/components/UsersList.tsx
+++ frontend/src/features/friends/components/UsersList.tsx
@@ -1,46 +1,77 @@
 import { FC, memo, useEffect } from 'react';
-import { Avatar } from '@chakra-ui/react';
-import { User } from '@prisma/client';
-import { Link } from 'react-router-dom';
-import { PrimaryTable } from 'components/table/PrimaryTable';
+import { Wrap, WrapItem } from '@chakra-ui/react';
 import { useAllUsers } from '../hooks/useAllUsers';
-import { AddFriend } from './AddFriend';
-import { DirectMessageButton } from './DirectMessageButton';
+import { FriendCard } from './FriendCard';
 
 export const UsersList: FC = memo(() => {
   const { getUsers, users } = useAllUsers();
 
   useEffect(() => getUsers(), [getUsers]);
 
+  const onClickAddFriend = () => {
+    alert('onClickAddFriend');
+  };
+
   return (
-    <PrimaryTable<User>
-      data={users}
-      columns={[
-        {
-          title: '',
-          Cell({ entry: { avatarUrl } }) {
-            return <Avatar size="md" src={avatarUrl} />;
-          },
-        },
-        {
-          title: '',
-          Cell({ entry: { name } }) {
-            return <Link to={`../${name}`}>{name}</Link>;
-          },
-        },
-        {
-          title: '',
-          Cell({ entry: { name } }) {
-            return <DirectMessageButton id={name} />;
-          },
-        },
-        {
-          title: '',
-          Cell({ entry: { name } }) {
-            return <AddFriend id={name} />;
-          },
-        },
-      ]}
-    />
+    <Wrap p={{ base: 4, md: 10 }}>
+      {users.map((obj) => (
+        <WrapItem key={obj.id} mx="auto">
+          <FriendCard
+            id={obj.id}
+            avatarUrl={obj.avatarUrl}
+            name={obj.name}
+            button="Add"
+            onClick={onClickAddFriend}
+          />
+        </WrapItem>
+      ))}
+    </Wrap>
   );
 });
+
+// import { FC, memo, useEffect } from 'react';
+// import { Avatar } from '@chakra-ui/react';
+// import { User } from '@prisma/client';
+// import { Link } from 'react-router-dom';
+// import { PrimaryTable } from 'components/table/PrimaryTable';
+// import { useAllUsers } from '../hooks/useAllUsers';
+// import { AddFriend } from './AddFriend';
+// import { DirectMessageButton } from './DirectMessageButton';
+
+// export const UsersList: FC = memo(() => {
+//   const { getUsers, users } = useAllUsers();
+
+//   useEffect(() => getUsers(), [getUsers]);
+
+//   return (
+//     <PrimaryTable<User>
+//       data={users}
+//       columns={[
+//         {
+//           title: '',
+//           Cell({ entry: { avatarUrl } }) {
+//             return <Avatar size="md" src={avatarUrl} />;
+//           },
+//         },
+//         {
+//           title: '',
+//           Cell({ entry: { name } }) {
+//             return <Link to={`../${name}`}>{name}</Link>;
+//           },
+//         },
+//         {
+//           title: '',
+//           Cell({ entry: { name } }) {
+//             return <DirectMessageButton id={name} />;
+//           },
+//         },
+//         {
+//           title: '',
+//           Cell({ entry: { name } }) {
+//             return <AddFriend id={name} />;
+//           },
+//         },
+//       ]}
+//     />
+//   );
+// });
