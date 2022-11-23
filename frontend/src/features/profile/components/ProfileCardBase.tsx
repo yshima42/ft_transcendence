f02845632733bd1ff1ecdd0fecbaaca4b6c8867a
Masaya Kamei <km9smc6@gmail.com>
2022-11-23 14:21:21 +0900

    userinfo簡単なやつ追加

---
+++ frontend/src/features/profile/components/ProfileCardBase.tsx
@@ -0,0 +1,23 @@
+import { memo, FC, Suspense, PropsWithChildren } from 'react';
+import { Center, Spinner } from '@chakra-ui/react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Navigate } from 'react-router-dom';
+
+export const ProfileCardBase: FC<PropsWithChildren> = memo((props) => {
+  const { children } = props;
+
+  return (
+    // TODO: alert 出したい。あとContentLayout に入れてもいいかも
+    <ErrorBoundary fallback={<Navigate to="." replace={true} />}>
+      <Suspense
+        fallback={
+          <Center h="100%">
+            <Spinner emptyColor="gray.200" color="blue.500" size="xl" />
+          </Center>
+        }
+      >
+        {children}
+      </Suspense>
+    </ErrorBoundary>
+  );
+});
