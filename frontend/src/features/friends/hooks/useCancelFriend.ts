9a15e4d5a4ab7d5372028cc04f7d21b8b0b8db46
kohkubo <kohkubo@student.42tokyo.jp>
2022-11-24 17:37:00 +0900

    hookのtry catchを消去

--- frontend/src/features/friends/hooks/useCancelFriend.ts
+++ frontend/src/features/friends/hooks/useCancelFriend.ts
@@ -3,15 +3,11 @@ import { axios } from 'lib/axios';
 
 export const useCancelFriend = (): { cancelFriend: (id: string) => void } => {
   async function cancelFriend(id: string): Promise<void> {
-    try {
-      await axios.delete<User[]>('/friendships/cancel', {
-        data: {
-          peerId: id,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    await axios.delete<User[]>('/friendships/cancel', {
+      data: {
+        peerId: id,
+      },
+    });
   }
 
   return { cancelFriend };
