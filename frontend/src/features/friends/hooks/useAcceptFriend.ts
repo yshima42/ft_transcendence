fd8f121fe731375c79978d8d1a6ddda5ef5f03f9
kohkubo <kohkubo@student.42tokyo.jp>
2022-11-24 17:37:00 +0900

    hookのtry catchを消去

--- frontend/src/features/friends/hooks/useAcceptFriend.ts
+++ frontend/src/features/friends/hooks/useAcceptFriend.ts
@@ -3,13 +3,9 @@ import { axios } from 'lib/axios';
 
 export const useAcceptFriend = (): { acceptFriend: (id: string) => void } => {
   async function acceptFriend(id: string): Promise<void> {
-    try {
-      await axios.post<User[]>('/friendships/accept', {
-        peerId: id,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    await axios.post<User[]>('/friendships/accept', {
+      peerId: id,
+    });
   }
 
   return { acceptFriend };
--- frontend/src/features/friends/hooks/useRequestFriend.ts
+++ frontend/src/features/friends/hooks/useRequestFriend.ts
@@ -1,19 +1,11 @@
 import { User } from '@prisma/client';
 import { axios } from 'lib/axios';
 
-// curl --location --request POST 'http://localhost:3000/friendships/request' \
-// --header 'Content-Type: application/x-www-form-urlencoded' \
-// --header 'Cookie: access_token= eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIxNTE0ZDhiLWU2YWYtNDkwYy1iYzUxLWQwYzdhMzU5YTI2NyIsIm5hbWUiOiJkdW1teTEiLCJpYXQiOjE2NjkxOTIyMjQsImV4cCI6MTY2OTI3ODYyNH0.uy-7gzMA3FwRB3cx3kYsKEik14ZuEMimgsmTgGoo2Q4' \
-// --data-urlencode 'peerId=5001da8b-0316-411e-a34f-1db29d4d4c4b'
 export const useRequestFriend = (): { requestFriend: (id: string) => void } => {
   async function requestFriend(id: string): Promise<void> {
-    try {
-      await axios.post<User[]>('/friendships/request', {
-        peerId: id,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    await axios.post<User[]>('/friendships/request', {
+      peerId: id,
+    });
   }
 
   return { requestFriend };
