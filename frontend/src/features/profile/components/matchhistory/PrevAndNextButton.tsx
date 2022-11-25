fa0f7fe077701df29fee3153ec81466b8cce37e4
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-11-25 12:51:47 +0900

    background-color削除。

--- frontend/src/features/profile/components/matchhistory/PrevAndNextButton.tsx
+++ frontend/src/features/profile/components/matchhistory/PrevAndNextButton.tsx
@@ -4,7 +4,7 @@ import { Box, Button, Flex } from '@chakra-ui/react';
 export const PrevAndNextButton: FC = memo(() => {
   return (
     <Box h="80px">
-      <Flex bg="teal.200" justify="center" align="center">
+      <Flex justify="center" align="center">
         <Button mx={8}>prev</Button>
         <Button mx={8}>next</Button>
       </Flex>
