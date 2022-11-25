0c277493f430ee0d861fedab38ea38af07555c26
hyoshie_vm <dymn9.term@gmail.com>
2022-11-25 12:01:10 +0900

    refactor: ブロックAPIのエンドポイントを修正

--- backend/src/blocks/blocks.module.ts
+++ backend/src/blocks/blocks.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { BlocksController } from './blocks.controller';
 import { BlocksService } from './blocks.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [BlocksController],
   providers: [BlocksService],
+  exports: [BlocksService],
 })
 export class BlocksModule {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { BlocksModule } from 'src/blocks/blocks.module';
 import { FileModule } from 'src/file/file.module';
 import { GameModule } from 'src/game/game.module';
 import { AuthModule } from '../auth/auth.module';
@@ -7,7 +8,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, FileModule, GameModule],
+  imports: [AuthModule, PrismaModule, FileModule, GameModule, BlocksModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
