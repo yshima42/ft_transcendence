dba7146d00574e82a7eab65172078c0cc480ffa6
hyoshie_vm <dymn9.term@gmail.com>
2022-11-26 18:11:14 +0900

    refactor: blockServiceのテストをリファクタ

--- backend/src/blocks/blocks.service.spec.ts
+++ backend/src/blocks/blocks.service.spec.ts
@@ -5,39 +5,60 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { BlocksService } from './blocks.service';
 
+const date = new Date('2022-11-01T04:34:22+09:00');
+
+const userArray: User[] = [
+  {
+    id: '21514d8b-e6af-490c-bc51-d0c7a359a267',
+    createdAt: new Date(date),
+    updatedAt: new Date(date),
+    name: 'blockDummy1',
+    avatarImageUrl: 'https://placehold.jp/2b52ee/ffffff/150x150.png?text=1',
+    nickname: 'nickname1',
+    onlineStatus: 'ONLINE',
+  },
+  {
+    id: '40e8b4b4-9b39-4b7e-8e31-78e31975d320',
+    createdAt: new Date(date),
+    updatedAt: new Date(date),
+    name: 'blockDummy2',
+    avatarImageUrl: 'https://placehold.jp/2b52ee/ffffff/150x150.png?text=2',
+    nickname: 'nickname2',
+    onlineStatus: 'OFFLINE',
+  },
+  {
+    id: '5001da8b-0316-411e-a34f-1db29d4d4c4b',
+    createdAt: new Date(date),
+    updatedAt: new Date(date),
+    name: 'blockDummy3',
+    avatarImageUrl: 'https://placehold.jp/2b52ee/ffffff/150x150.png?text=3',
+    nickname: 'nickname3',
+    onlineStatus: 'INGAME',
+  },
+];
+
+const oneUser = userArray[0];
+
+const blockArray: Block[] = [
+  {
+    sourceId: userArray[0].id,
+    targetId: userArray[1].id,
+  },
+  {
+    sourceId: userArray[0].id,
+    targetId: userArray[2].id,
+  },
+];
+
+const oneBlock: Block = blockArray[0];
+const nonExistBlock: Block = {
+  sourceId: userArray[1].id,
+  targetId: userArray[2].id,
+};
+
 describe('BlocksService', () => {
   let blocksService: BlocksService;
   let prisma: PrismaService;
-  const date = new Date('2022-11-01T04:34:22+09:00');
-  const mockUsers: User[] = [
-    {
-      id: '21514d8b-e6af-490c-bc51-d0c7a359a267',
-      createdAt: new Date(date),
-      updatedAt: new Date(date),
-      name: 'blockDummy1',
-      avatarImageUrl: 'https://placehold.jp/2b52ee/ffffff/150x150.png?text=1',
-      nickname: 'nickname1',
-      onlineStatus: 'ONLINE',
-    },
-    {
-      id: '40e8b4b4-9b39-4b7e-8e31-78e31975d320',
-      createdAt: new Date(date),
-      updatedAt: new Date(date),
-      name: 'blockDummy2',
-      avatarImageUrl: 'https://placehold.jp/2b52ee/ffffff/150x150.png?text=2',
-      nickname: 'nickname2',
-      onlineStatus: 'OFFLINE',
-    },
-    {
-      id: '5001da8b-0316-411e-a34f-1db29d4d4c4b',
-      createdAt: new Date(date),
-      updatedAt: new Date(date),
-      name: 'blockDummy3',
-      avatarImageUrl: 'https://placehold.jp/2b52ee/ffffff/150x150.png?text=3',
-      nickname: 'nickname3',
-      onlineStatus: 'INGAME',
-    },
-  ];
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -49,9 +70,16 @@ describe('BlocksService', () => {
     blocksService = module.get<BlocksService>(BlocksService);
   });
 
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
   beforeEach(async () => {
     await prisma.user.createMany({
-      data: mockUsers,
+      data: userArray,
+    });
+    await prisma.block.createMany({
+      data: blockArray,
     });
   });
 
@@ -60,122 +88,54 @@ describe('BlocksService', () => {
       prisma.block.deleteMany(),
       prisma.user.deleteMany(),
     ]);
-    await prisma.$disconnect();
   });
 
   it('should be defined', () => {
     expect(blocksService).toBeDefined();
   });
 
-  it('should create block', async () => {
-    const newBlock = await blocksService.create(
-      mockUsers[0].id,
-      mockUsers[1].id
-    );
-
-    expect(newBlock).toHaveProperty('sourceId', mockUsers[0].id);
-    expect(newBlock).toHaveProperty('targetId', mockUsers[1].id);
-  });
-
-  it('should not block myself', async () => {
-    const createFunc = async () => {
-      return await blocksService.create(mockUsers[0].id, mockUsers[0].id);
-    };
-
-    await expect(createFunc()).rejects.toThrow(BadRequestException);
-  });
-
-  it('should not create duplicated block', async () => {
-    const createFunc = async () => {
-      return await blocksService.create(mockUsers[0].id, mockUsers[1].id);
-    };
-    const newBlock: Block = {
-      sourceId: mockUsers[0].id,
-      targetId: mockUsers[1].id,
-    };
-
-    await expect(createFunc()).resolves.toStrictEqual(newBlock);
-    await expect(createFunc()).rejects.toThrow(BadRequestException);
-  });
-
-  it('should remove block', async () => {
-    const _newBlock = await prisma.block.create({
-      data: {
-        sourceId: mockUsers[0].id,
-        targetId: mockUsers[1].id,
-      },
+  describe('create', () => {
+    it('should successfully block', async () => {
+      await expect(
+        blocksService.create(nonExistBlock.sourceId, nonExistBlock.targetId)
+      ).resolves.toStrictEqual(nonExistBlock);
     });
-    const recordCountBefore = await prisma.block.count();
-    const deletedRequest = await blocksService.remove(
-      mockUsers[0].id,
-      mockUsers[1].id
-    );
-    const recordCountAfter = await prisma.block.count();
 
-    expect(deletedRequest).toHaveProperty('sourceId', mockUsers[0].id);
-    expect(recordCountBefore).toBe(1);
-    expect(recordCountAfter).toBe(0);
-  });
-
-  // テスト名が微妙。のちほど修正
-  it('should not remove non-existent block', async () => {
-    const deleteFunc = async () =>
-      await blocksService.remove(mockUsers[0].id, mockUsers[1].id);
-    await expect(deleteFunc()).rejects.toThrow(NotFoundException);
-  });
-
-  it('should find a blocking user', async () => {
-    const _newBlocks = await prisma.block.createMany({
-      data: [
-        {
-          sourceId: mockUsers[0].id,
-          targetId: mockUsers[1].id,
-        },
-        {
-          sourceId: mockUsers[1].id,
-          targetId: mockUsers[2].id,
-        },
-        {
-          sourceId: mockUsers[2].id,
-          targetId: mockUsers[1].id,
-        },
-        {
-          sourceId: mockUsers[2].id,
-          targetId: mockUsers[0].id,
-        },
-      ],
+    it('should not block myself', async () => {
+      await expect(
+        blocksService.create(oneUser.id, oneUser.id)
+      ).rejects.toThrow(BadRequestException);
     });
-    const blockedUsers = await blocksService.findBlockedUsers(mockUsers[0].id);
 
-    expect(blockedUsers).toHaveLength(1);
-    expect(blockedUsers).toEqual([mockUsers[1]]);
+    it('should not create duplicated block', async () => {
+      await expect(
+        blocksService.create(oneBlock.sourceId, oneBlock.targetId)
+      ).rejects.toThrow(BadRequestException);
+    });
   });
 
-  it('should find some blocking users', async () => {
-    const _newBlocks = await prisma.block.createMany({
-      data: [
-        {
-          sourceId: mockUsers[0].id,
-          targetId: mockUsers[1].id,
-        },
-        {
-          sourceId: mockUsers[1].id,
-          targetId: mockUsers[2].id,
-        },
-        {
-          sourceId: mockUsers[2].id,
-          targetId: mockUsers[1].id,
-        },
-        {
-          sourceId: mockUsers[2].id,
-          targetId: mockUsers[0].id,
-        },
-      ],
+  describe('findBlockedUsers', () => {
+    it('should find blocking users', async () => {
+      await expect(
+        blocksService.findBlockedUsers(oneUser.id)
+      ).resolves.toStrictEqual([userArray[1], userArray[2]]);
     });
-    const blockedUsers = await blocksService.findBlockedUsers(mockUsers[2].id);
+  });
 
-    expect(blockedUsers).toHaveLength(2);
-    expect(blockedUsers).toContainEqual(mockUsers[0]);
-    expect(blockedUsers).toContainEqual(mockUsers[1]);
+  describe('remove', () => {
+    it('should remove block', async () => {
+      const removeFunc = async () => {
+        return await blocksService.remove(oneBlock.sourceId, oneBlock.targetId);
+      };
+
+      await expect(removeFunc()).resolves.toStrictEqual(oneBlock);
+    });
+
+    // テスト名が微妙。のちほど修正
+    it('should not remove non-existent block', async () => {
+      await expect(
+        blocksService.remove(nonExistBlock.sourceId, nonExistBlock.targetId)
+      ).rejects.toThrow(NotFoundException);
+    });
   });
 });
