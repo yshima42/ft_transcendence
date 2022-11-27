eed50d64f63e593ad76b172700224841fcf3facf
kohkubo <kohkubo@student.42tokyo.jp>
2022-11-27 18:55:41 +0900

    prisma generate関連

--- backend/prisma/migrations/20221127092151_/migration.sql
+++ backend/prisma/migrations/20221127092151_/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" TEXT NOT NULL,
