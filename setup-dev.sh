a773ec2670de17a357a13a3cefd7518e1e5f8601
Masaya Kamei <km9smc6@gmail.com>
2022-11-28 11:16:44 +0900

    pre-merge,checkout スクリプト追加

---
+++ setup-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+projects=("backend" "frontend")
+rootDir=$(pwd | sed -r "s/\/\.git\/hooks//")
+
+for project in ${projects[@]}; do
+  echo "Executing $project setup:dev"
+  cd "$rootDir/$project"
+  yarn setup:dev 2>/dev/null
+done
