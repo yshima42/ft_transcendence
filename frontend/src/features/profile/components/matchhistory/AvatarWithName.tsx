07aa1d9c27b99f7f3decca8f74a5752c5b35b151
hyoshie_vm <dymn9.term@gmail.com>
2022-11-30 15:13:11 +0900

    refactor: avatarUrl変数をavatarImageUrlに統一した

--- frontend/src/features/profile/components/matchhistory/AvatarWithName.tsx
+++ frontend/src/features/profile/components/matchhistory/AvatarWithName.tsx
@@ -3,16 +3,16 @@ import { Avatar, Box, Text, VStack } from '@chakra-ui/react';
 
 type Props = {
   name: string;
-  avatarUrl: string;
+  avatarImageUrl: string;
 };
 
 export const AvatarWithName: FC<Props> = memo((props) => {
-  const { name, avatarUrl } = props;
+  const { name, avatarImageUrl } = props;
 
   return (
     <Box h="90" p={2}>
       <VStack>
-        <Avatar size="md" src={avatarUrl} name={name} />
+        <Avatar size="md" src={avatarImageUrl} name={name} />
         {/* TODO:ニックネームが長くなるときどう表示するか考える。暫定で省略している */}
         <Text maxWidth={20} noOfLines={1}>
           {name}
