4fe3d02da2bd81c8e620c34c2cf3e0d1d9bda80d
yuki <shimazuyuki7@gmail.com>
2022-12-01 17:48:19 +0900

    refactor: PongGameの変数設定をリファクター

--- frontend/src/features/game/components/Canvas.tsx
+++ frontend/src/features/game/components/Canvas.tsx
@@ -3,11 +3,13 @@ import { useCanvas } from '../hooks/useCanvas';
 
 type Props = {
   draw: (ctx: CanvasRenderingContext2D, frameCount: number) => void;
+  width: number;
+  height: number;
 };
 
 export const Canvas: FC<Props> = memo((props) => {
-  const { draw } = props;
+  const { draw, width, height } = props;
   const canvasRef = useCanvas(draw);
 
-  return <canvas width={1000} height={500} ref={canvasRef} />;
+  return <canvas width={width} height={height} ref={canvasRef} />;
 });
--- frontend/src/features/game/components/PongGame.tsx
+++ frontend/src/features/game/components/PongGame.tsx
@@ -1,18 +1,26 @@
 import { memo, FC, useCallback } from 'react';
 import { Canvas } from './Canvas';
 
+// ToDo: これらを設定などで変えられる機能を作る
 const BALL_START_X = 50;
 const BALL_START_Y = 100;
+const BALL_SPEED = 5;
+const BALL_SIZE = 10;
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = 500;
+const PADDLE_WIDTH = 20;
+const PADDLE_HEIGHT = 75;
+const PADDLE_START_POS = (CANVAS_HEIGHT - PADDLE_HEIGHT) / 2;
+const PADDLE_SPEED = 7;
 
 export const PongGame: FC = memo(() => {
-  let x = BALL_START_X;
-  let y = BALL_START_Y;
-  let dx = 5;
-  let dy = -5;
-  const ballRadius = 10;
-  const paddleHeight = 75;
-  const paddleWidth = 20;
-
+  let ballX = BALL_START_X;
+  let ballY = BALL_START_Y;
+  let dX = BALL_SPEED;
+  let dY = -BALL_SPEED;
+  const ballRadius = BALL_SIZE;
+  const paddleHeight = PADDLE_HEIGHT;
+  const paddleWidth = PADDLE_WIDTH;
   let rightPressed = false;
   let leftPressed = false;
 
@@ -38,21 +46,21 @@ export const PongGame: FC = memo(() => {
   const drawBall = (ctx: CanvasRenderingContext2D) => {
     ctx.fillStyle = '#4FD1C5';
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, 2 * Math.PI);
+    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
     ctx.fill();
   };
 
-  let paddleY = 500 / 2;
+  let paddleY = PADDLE_START_POS;
   const drawPaddle = (ctx: CanvasRenderingContext2D) => {
     ctx.beginPath();
 
     if (rightPressed) {
-      paddleY += 7;
+      paddleY += PADDLE_SPEED;
       if (paddleY + paddleHeight > ctx.canvas.height) {
         paddleY = ctx.canvas.height - paddleHeight;
       }
     } else if (leftPressed) {
-      paddleY -= 7;
+      paddleY -= PADDLE_SPEED;
       if (paddleY < 0) {
         paddleY = 0;
       }
@@ -72,12 +80,12 @@ export const PongGame: FC = memo(() => {
     drawBall(ctx);
     drawPaddle(ctx);
 
-    // 壁で跳ね返る処理
-    if (x + dx > ctx.canvas.width - ballRadius) {
-      dx = -dx;
-    } else if (x + dx < ballRadius) {
-      if (y > paddleY && y < paddleY + paddleHeight) {
-        dx = -dx;
+    // パドルで跳ね返る処理・ゲームオーバー処理
+    if (ballX + dX > ctx.canvas.width - ballRadius) {
+      dX = -dX;
+    } else if (ballX + dX < ballRadius) {
+      if (ballY > paddleY && ballY < paddleY + paddleHeight) {
+        dX = -dX;
       } else {
         console.log('Game Over');
         document.location.reload();
@@ -85,14 +93,18 @@ export const PongGame: FC = memo(() => {
       }
     }
 
-    if (y + dy > ctx.canvas.height - ballRadius || y + dy < ballRadius) {
-      dy = -dy;
+    // 上下の壁で跳ね返る処理
+    if (
+      ballY + dY > ctx.canvas.height - ballRadius ||
+      ballY + dY < ballRadius
+    ) {
+      dY = -dY;
     }
 
     // frameごとに進む
-    x += dx;
-    y += dy;
+    ballX += dX;
+    ballY += dY;
   }, []);
 
-  return <Canvas draw={draw} />;
+  return <Canvas draw={draw} width={CANVAS_WIDTH} height={CANVAS_HEIGHT} />;
 });
