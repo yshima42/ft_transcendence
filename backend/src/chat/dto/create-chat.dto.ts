5d5a3b7dd3e638d02efcf6e079934fadf1a6fe4b
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-03 01:46:49 +0900

    dto update

--- backend/src/chat/dto/create-chat.dto.ts
+++ backend/src/chat/dto/create-chat.dto.ts
@@ -1,62 +1,36 @@
 import { ChatUserStatus } from '@prisma/client';
-import * as CV from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID, IsEnum } from 'class-validator';
 
-// model ChatRoom {
-//   id           String        @id @default(uuid())
-//   name         String        @unique
-//   createdAt    DateTime      @default(now())
-//   updatedAt    DateTime      @updatedAt
-//   chatMessages ChatMessage[] // チャットルームに属するメッセージ
-//   chatUsers    ChatUser[] // チャットルームに属するユーザー
-// }
 export class CreateChatRoomDto {
-  @CV.IsNotEmpty()
-  @CV.IsString()
+  @IsNotEmpty()
+  @IsString()
   name!: string;
 }
 
-// model ChatMessage {
-//   id         String   @id @default(uuid())
-//   content    String
-//   createdAt  DateTime @default(now())
-//   chatRoom   ChatRoom @relation(fields: [chatRoomId], references: [id]) // 属するチャットルーム
-//   chatRoomId String
-//   sender     User     @relation("messageSender", fields: [senderId], references: [id]) // メッセージに属するユーザー
-//   senderId   String
-// }
 export class CreateMessageDto {
-  @CV.IsNotEmpty()
-  @CV.IsString()
+  @IsNotEmpty()
+  @IsString()
   content!: string;
 
-  @CV.IsNotEmpty()
-  @CV.IsUUID()
+  @IsNotEmpty()
+  @IsUUID()
   chatRoomId!: string;
 
-  @CV.IsNotEmpty()
-  @CV.IsUUID()
+  @IsNotEmpty()
+  @IsUUID()
   senderId!: string;
 }
 
-// model ChatUser {
-//   chatRoom   ChatRoom       @relation(fields: [chatRoomId], references: [id]) // 参加しているチャットルーム
-//   chatRoomId String
-//   user       User           @relation(fields: [userId], references: [id]) // チャットの参加者
-//   userId     String
-//   status     ChatUserStatus // チャットの参加者のステータス
-
-//   @@id([chatRoomId, userId])
-// }
 export class CreateChatUserDto {
-  @CV.IsNotEmpty()
-  @CV.IsUUID()
+  @IsNotEmpty()
+  @IsUUID()
   chatRoomId!: string;
 
-  @CV.IsNotEmpty()
-  @CV.IsUUID()
+  @IsNotEmpty()
+  @IsUUID()
   userId!: string;
 
-  @CV.IsNotEmpty()
-  @CV.IsEnum(ChatUserStatus)
+  @IsNotEmpty()
+  @IsEnum(ChatUserStatus)
   status!: ChatUserStatus;
 }
