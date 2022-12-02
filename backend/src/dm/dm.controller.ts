95f7204da66d5c23c46e9493132eb5d17d7ab5a3
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-02 22:45:57 +0900

    dm関連backendとりあえず

--- backend/src/dm/dm.controller.ts
+++ backend/src/dm/dm.controller.ts
@@ -9,11 +9,13 @@ import {
   UseGuards,
   Logger,
 } from '@nestjs/common';
-import { User, DmRoom, DmMessage } from '@prisma/client';
+import { User, DmMessage } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ResponseDmRoom, ResponseDmMessage } from './dm.interface';
 import { DmService } from './dm.service';
 import { CreateDmMessageDto } from './dto/create-dm.dto';
+
 // import { UpdateDmDto } from './dto/update-dm.dto';
 
 @Controller('dm')
@@ -24,11 +26,12 @@ export class DmController {
   @Post(':id')
   @UseGuards(JwtAuthGuard)
   async create(
-    @Body() createDmMessageDto: CreateDmMessageDto
+    @Body() createDmMessageDto: CreateDmMessageDto,
+    @GetUser() user: User
   ): Promise<DmMessage> {
-    Logger.verbose(`createDmMessageDto: ${JSON.stringify(createDmMessageDto)}`);
+    Logger.debug(`createDmMessageDto: ${JSON.stringify(createDmMessageDto)}`);
 
-    return await this.dmService.create(createDmMessageDto);
+    return await this.dmService.create(createDmMessageDto, user.id);
   }
 
   // @Get()
@@ -38,13 +41,15 @@ export class DmController {
 
   @Get('messages/:id')
   @UseGuards(JwtAuthGuard)
-  async findDmMessages(@Param('id') id: string): Promise<DmMessage[]> {
+  async findDmMessages(@Param('id') id: string): Promise<ResponseDmMessage[]> {
     return await this.dmService.findDmMessages(id);
   }
 
   @Get('me')
   @UseGuards(JwtAuthGuard)
-  async findMyDmRooms(@GetUser() user: User): Promise<DmRoom[]> {
+  async findMyDmRooms(@GetUser() user: User): Promise<ResponseDmRoom[]> {
+    Logger.debug(`findMyDmRooms: ${JSON.stringify(user)}`);
+
     return await this.dmService.findMyDmRooms(user.id);
   }
 
