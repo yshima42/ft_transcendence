eb0f6e9803a5d982e0293b305005de3f347c8e9c
hyoshie_vm <dymn9.term@gmail.com>
2022-12-02 13:57:38 +0900

    fix: MatchHistoryから他ユーザーのプロフィールに遷移できるようにした

--- frontend/src/features/profile/components/matchhistory/AvatarWithName.tsx
+++ frontend/src/features/profile/components/matchhistory/AvatarWithName.tsx
@@ -1,18 +1,27 @@
 import { memo, FC } from 'react';
-import { Avatar, Box, Text, VStack } from '@chakra-ui/react';
+import { Box, Text, VStack } from '@chakra-ui/react';
+import { LinkedAvatar } from 'components/atoms/avatar/LinkedAvatar';
 
 type Props = {
   name: string;
   avatarImageUrl: string;
+  id: string;
 };
 
 export const AvatarWithName: FC<Props> = memo((props) => {
-  const { name, avatarImageUrl } = props;
+  const { name, avatarImageUrl, id } = props;
 
   return (
     <Box h="90" p={2}>
       <VStack>
-        <Avatar size="md" src={avatarImageUrl} name={name} />
+        <LinkedAvatar
+          size="md"
+          src={avatarImageUrl}
+          name={name}
+          linkUrl={`/app/users/${id}`}
+          id={id}
+        />
+
         {/* TODO:ニックネームが長くなるときどう表示するか考える。暫定で省略している */}
         <Text maxWidth={20} noOfLines={1}>
           {name}
