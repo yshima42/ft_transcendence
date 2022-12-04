0131b197a2004acd12aaaa9198848f635d360078
Masaya Kamei <km9smc6@gmail.com>
2022-12-04 16:54:49 +0900

    Add useQrCodeUrl

--- frontend/src/hooks/api/generics/useGetApi.ts
+++ frontend/src/hooks/api/generics/useGetApi.ts
@@ -1,22 +1,33 @@
-import { useQuery } from '@tanstack/react-query';
+import {
+  QueryObserverResult,
+  RefetchOptions,
+  RefetchQueryFilters,
+  useQuery,
+} from '@tanstack/react-query';
 import { axios } from '../../../lib/axios';
 
 // 使う際は、このhooks自体を<Suspense> で囲むこと。
 // エラーをキャッチしたい場合は、<ErrorBoundary> で囲むこと。
-export function useGetApi<ResBody>(endpoint: string): {
+export function useGetApi<ResBody>(
+  endpoint: string,
+  options?: { enabled?: boolean; refetchOnWindowFocus?: boolean }
+): {
   data: ResBody;
+  refetch: <TPageData>(
+    options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined
+  ) => Promise<QueryObserverResult<ResBody, unknown>>;
 } {
   const axiosGet = async (): Promise<ResBody> => {
     const result = await axios.get<ResBody>(endpoint);
 
     return result.data;
   };
-  const { data } = useQuery<ResBody>([endpoint], axiosGet);
+  const { data, refetch } = useQuery<ResBody>([endpoint], axiosGet, options);
 
   // TODO: エラーの場合、useQuery内で例外が投げられるので、いつ入るかわかってない。
   if (data === undefined) {
     throw new Error('Error in userProfile');
   }
 
-  return { data };
+  return { data, refetch };
 }
