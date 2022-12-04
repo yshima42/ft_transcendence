cf69e3129d0e1c3f9f21e8cd5a1b5941fdad2e48
yuki <shimazuyuki7@gmail.com>
2022-12-04 21:53:14 +0900

    chore: コネクション確認

--- backend/src/events/pong.gateway.ts
+++ backend/src/events/pong.gateway.ts
@@ -10,69 +10,33 @@ import { Socket, Server } from 'socket.io';
 
 // const games: Socket[] = [];
 
+type Players = {
+  [key: string]: string;
+};
+
+const players: Players = {};
+
 @WebSocketGateway({ cors: { origin: '*' } })
 export class PongGateway {
   @WebSocketServer()
   server!: Server;
 
-  handleConnection(@ConnectedSocket() socket: Socket): void {
-    console.log(socket.id);
-  }
-
   private readonly logger: Logger = new Logger('Gateway Log');
 
-  // @SubscribeMessage('message')
-  // handleMessage(
-  //   @MessageBody() message: string,
-  //   @ConnectedSocket() socket: Socket
-  // ): void {
-  //   this.logger.log(`message recieved ${message}`);
-  //   const rooms = [...socket.rooms].slice(0);
-  //   this.server.to(rooms[1]).emit('update', message);
-  // }
-
-  @SubscribeMessage('joinRoom')
-  async joinRoom(
-    @MessageBody() roomId: string,
-    @ConnectedSocket() socket: Socket
-  ): Promise<void> {
-    this.logger.log(`joinRoom: ${socket.id} joined ${roomId}`);
-
-    const connectedSockets = this.server.sockets.adapter.rooms.get(roomId);
-    const socketRooms = Array.from(socket.rooms.values()).filter(
-      (r) => r !== socket.id
-    );
-
-    if (
-      socketRooms.length > 0 ||
-      (connectedSockets != null && connectedSockets.size === 2)
-    ) {
-      socket.emit('roomJoinError', {
-        error: 'Room is full',
-      });
-    } else {
-      await socket.join(roomId);
-      socket.emit('roomJoined');
-    }
-  }
-
-  private getSocketGameRoom(socket: Socket): string {
-    const socketRooms = Array.from(socket.rooms.values()).filter(
-      (r) => r !== socket.id
-    );
-    const gameRoom = socketRooms?.[0];
-
-    return gameRoom;
-  }
-
-  @SubscribeMessage('update')
-  handleUpdate(
-    @MessageBody() message: string,
+  @SubscribeMessage('newPlayer')
+  handleNewPlayer(
+    @MessageBody() data: string,
     @ConnectedSocket() socket: Socket
   ): void {
-    const gameRoom = this.getSocketGameRoom(socket);
-    console.log(message);
-    socket.to(gameRoom).emit('onGameUpdate', message);
+    console.log(`new client: ${socket.id}`);
+    players[socket.id] = data;
+  }
+
+  @SubscribeMessage('disconnect')
+  handleDisconnect(@ConnectedSocket() socket: Socket): void {
+    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+    delete players[socket.id];
+    console.log(`good-bye client: ${socket.id}`);
   }
 
   // @SubscribeMessage('events')
