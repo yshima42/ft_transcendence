333ae0fe3b43f6c4bc976bc2130850dd69b6d73e
yuki <shimazuyuki7@gmail.com>
2022-12-06 09:47:04 +0900

    chore: 一旦色々削除して整理

--- frontend/src/features/game/components/PongGame.tsx
+++ frontend/src/features/game/components/PongGame.tsx
@@ -3,7 +3,6 @@ import { SOCKET_URL } from 'config/default';
 import { io } from 'socket.io-client';
 import {
   BALL_SIZE,
-  BALL_SPEED,
   BALL_START_X,
   BALL_START_Y,
   BG_COLOR,
@@ -23,36 +22,10 @@ const scoring = (player: Paddle) => {
   player.score++;
 };
 
-// type Players = {
-//   [key: string]: { x?: number; y?: number; up?: boolean; down?: boolean };
-// };
-
-// type ClientPaddles = {
-//   [key: string]: Paddle;
-// };
-
-// const clientPaddles: ClientPaddles = {};
-
 export const PongGame: FC = memo(() => {
-  // const startX = 40 + Math.random() * 400;
-  // const startY = 40 + Math.random() * 300;
-  let dX = BALL_SPEED;
-  let dY = -BALL_SPEED;
   const player1 = new Paddle(0, PADDLE_START_POS);
   const player2 = new Paddle(CANVAS_WIDTH - PADDLE_WIDTH, PADDLE_START_POS);
-  // const paddle2 = new Paddle(startX, startY);
   const ball = new Ball(BALL_START_X, BALL_START_Y);
-
-  // socket.emit('newPlayer', { x: startX, y: startY });
-  // socket.on('updatePlayers', (players: Players) => {
-  //   for (const id in players) {
-  //     console.log(id);
-  //     if (clientPaddles[id] === undefined && id !== socket.id) {
-  //       console.log(id);
-  //       clientPaddles[id] = new Paddle(100, 100);
-  //     }
-  //   }
-  // });
 
   const draw = useCallback((ctx: CanvasRenderingContext2D) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -64,12 +37,6 @@ export const PongGame: FC = memo(() => {
       player1.down = data.down;
     });
 
-    // socket.on('updatePos', (players: Players) => {
-    //   for (const id in players) {
-    //     console.log(id);
-    //     clientPaddles[id].draw(ctx);
-    //   }
-    // });
     player1.draw(ctx);
     player2.draw(ctx);
 
@@ -79,22 +46,22 @@ export const PongGame: FC = memo(() => {
 
     // パドルで跳ね返る処理・ゲームオーバー処理
     // TODO: 壁で跳ね返る処理はcanvasのwallを使えるかも
-    if (ball.pos.x + dX > ctx.canvas.width - BALL_SIZE) {
+    if (ball.pos.x + ball.dx > ctx.canvas.width - BALL_SIZE) {
       if (
         ball.pos.y > player2.pos.y &&
         ball.pos.y < player2.pos.y + PADDLE_HEIGHT
       ) {
-        dX = -dX;
+        ball.dx = -ball.dx;
       } else {
         scoring(player1);
         ball.setPosition(BALL_START_X, BALL_START_Y);
       }
-    } else if (ball.pos.x + dX < BALL_SIZE) {
+    } else if (ball.pos.x + ball.dx < BALL_SIZE) {
       if (
         ball.pos.y > player1.pos.y &&
         ball.pos.y < player1.pos.y + PADDLE_HEIGHT
       ) {
-        dX = -dX;
+        ball.dx = -ball.dx;
       } else {
         scoring(player2);
         ball.setPosition(BALL_START_X, BALL_START_Y);
@@ -114,15 +81,15 @@ export const PongGame: FC = memo(() => {
 
     // 上下の壁で跳ね返る処理
     if (
-      ball.pos.y + dY > ctx.canvas.height - BALL_SIZE ||
-      ball.pos.y + dY < BALL_SIZE
+      ball.pos.y + ball.dy > ctx.canvas.height - BALL_SIZE ||
+      ball.pos.y + ball.dy < BALL_SIZE
     ) {
-      dY = -dY;
+      ball.dy = -ball.dy;
     }
 
     // frameごとに進む
-    ball.pos.x += dX;
-    ball.pos.y += dY;
+    ball.pos.x += ball.dx;
+    ball.pos.y += ball.dy;
 
     // socket.emit('update', { x: paddle1.pos.x, y: paddle1.pos.y });
   }, []);
