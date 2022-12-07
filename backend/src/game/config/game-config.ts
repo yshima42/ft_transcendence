fbe5f99636126460c64147a6de8981829eb98386
yuki <shimazuyuki7@gmail.com>
2022-12-08 08:51:56 +0900

    chore: 得点を表示できるように

--- backend/src/game/config/game-config.ts
+++ backend/src/game/config/game-config.ts
@@ -1,4 +1,5 @@
 // TODO: これらを設定などで変えられる機能を作る
+// TODO: フロントと共通にしないといけない
 export const CANVAS_WIDTH = 1000;
 export const CANVAS_HEIGHT = 500;
 export const BALL_START_X = CANVAS_WIDTH / 2;
@@ -8,7 +9,7 @@ export const BALL_SIZE = 10;
 export const PADDLE_WIDTH = 20;
 export const PADDLE_HEIGHT = 75;
 export const PADDLE_START_POS = (CANVAS_HEIGHT - PADDLE_HEIGHT) / 2;
-export const PADDLE_SPEED = 4;
+export const PADDLE_SPEED = 32;
 export const BALL_COLOR = '#4FD1C5';
 export const PADDLE_COLOR = '#4FD1C5';
 export const BG_COLOR = '#1A202C';
---
+++ frontend/src/hooks/socket/useSocket.ts
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+import { io, ManagerOptions, Socket, SocketOptions } from 'socket.io-client';
+
+// TODO: 現在は使っていないが、chat等を作る時、こちらを使ってsocketを作るのが良いのかも。必要なければ削除。
+// 参考: https://www.youtube.com/watch?v=-aTWWl4klYE
+export const useSocket = (
+  uri: string,
+  opts?: Partial<ManagerOptions & SocketOptions> | undefined
+): Socket => {
+  const { current: socket } = useRef(io(uri, opts));
+
+  useEffect(() => {
+    return () => {
+      if (socket != null) socket.close();
+    };
+  }, [socket]);
+
+  return socket;
+};
