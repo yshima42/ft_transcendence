4021131e5e96fd3365bec78b9398f4259181590b
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-12-10 11:15:13 +0900

    ファイル名変更。

--- frontend/src/hooks/api/auth/useGetTwoFactorAuthState.ts
+++
@@ -1,7 +0,0 @@
-import { useGetApi } from '../generics/useGetApi';
-
-export const useGetTwoFactorAuthState = (): { twoFactorAuthState: boolean } => {
-  const { data: twoFactorAuthState } = useGetApi<boolean>(`/auth/2fa`);
-
-  return { twoFactorAuthState };
-};
