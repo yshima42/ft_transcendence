22de758d1684d677c883aa1fdeecb754cc68c086
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-11 11:05:10 +0900

    test環境整備

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        log(message) {
+          console.log(`[cy.log] ${message}`);
+          return null;
+        },
+      });
     },
     baseUrl: 'http://localhost:5173',
   },
