871e0364d9622aeea7858540885d9f0525ad7c52
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-12-12 16:14:15 +0900

    命名変更。

--- frontend/src/hooks/api/auth/useQrcodeUrl.ts
+++
@@ -1,7 +0,0 @@
-import { useGetApi } from '../generics/useGetApi';
-
-export const useQrcodeUrl = (): { qrcodeUrl: string } => {
-  const { data: qrcodeUrl } = useGetApi<{ qrcodeUrl: string }>(`/auth/2fa`);
-
-  return qrcodeUrl;
-};
--- frontend/src/hooks/api/auth/useTwoFactorAuthState.ts
+++
@@ -1,7 +0,0 @@
-import { useGetApi } from '../generics/useGetApi';
-
-export const useTwoFactorAuthState = (): { twoFactorAuthState: boolean } => {
-  const { data: twoFactorAuthState } = useGetApi<boolean>(`/auth/2fa/state`);
-
-  return { twoFactorAuthState };
-};
