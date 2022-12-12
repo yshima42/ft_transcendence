446c833143bf62daec80043756ac1cee767723b9
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-12 20:02:17 +0900

    BANとMUTEに時間指定を追加

---
+++ frontend/src/features/chat/components/organisms/ChatRoomUserActionModal.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import * as C from '@chakra-ui/react';
+
+// 1m, 1h, 1d, 1w, 1M, unlimited
+type limit = '1m' | '1h' | '1d' | '1w' | '1M' | 'unlimited';
+const limitList: limit[] = ['1m', '1h', '1d', '1w', '1M', 'unlimited'];
+
+type Props = {
+  isOpen: boolean;
+  onClick: (limit: limit) => void;
+  onClose: () => void;
+};
+
+export const ChatRoomUserActionModal: React.FC<Props> = React.memo(
+  ({ isOpen, onClick, onClose }) => {
+    return (
+      <C.Modal isOpen={isOpen} onClose={() => onClose()}>
+        <C.ModalOverlay />
+        <C.ModalContent>
+          <C.ModalHeader>Modal Title</C.ModalHeader>
+          <C.ModalCloseButton />
+          <C.ModalBody>
+            <C.Stack direction="column">
+              {limitList.map((limit) => (
+                <C.Button key={limit} onClick={() => onClick(limit)}>
+                  {limit}
+                </C.Button>
+              ))}
+            </C.Stack>
+          </C.ModalBody>
+          <C.ModalFooter>
+            <C.Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </C.Button>
+          </C.ModalFooter>
+        </C.ModalContent>
+      </C.Modal>
+    );
+  }
+);
