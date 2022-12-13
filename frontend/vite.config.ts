f61997936801b993f66e334f742da3c943038bbf
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-13 13:58:11 +0900

    テスト周りの環境設定をリセット

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), EnvironmentPlugin('all')],
+  plugins: [react(), tsconfigPaths()],
 });
