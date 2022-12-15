6b3839180eededf1d79b561e50dcbdef7c62d7f1
hyoshie_vm <dymn9.term@gmail.com>
2022-12-15 15:24:34 +0900

    refactor: OnlineUsersProviderリファクタ

--- frontend/src/providers/OnlineUsersProvider.tsx
+++ frontend/src/providers/OnlineUsersProvider.tsx
@@ -2,7 +2,6 @@ import {
   createContext,
   FC,
   PropsWithChildren,
-  useCallback,
   useEffect,
   useRef,
   useState,
@@ -12,20 +11,26 @@ import { useSocket } from 'hooks/socket/useSocket';
 
 export const OnlineUsersContext = createContext<string[]>([]);
 
-const OnlineUsersProvider: FC<PropsWithChildren> = (props) => {
-  const { children } = props;
+const OnlineUsersProvider: FC<PropsWithChildren> = ({ children }) => {
   const { user } = useProfile();
-  // Strictモードによって2回発火するのを防ぐ
-  // https://www.sunapro.com/react18-strict-mode/#index_id5
-  const didLogRef = useRef(false);
-
   const socket = useSocket('http://localhost:3000/', {
     autoConnect: false,
   });
-
   const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
+  const didLogRef = useRef(false);
 
-  const startListeners = useCallback(() => {
+  useEffect(() => {
+    socket.connect();
+
+    // Strictモードによって2回発火するのを防ぐ
+    // https://www.sunapro.com/react18-strict-mode/#index_id5
+    if (!didLogRef.current) {
+      didLogRef.current = true;
+      socket.emit('handshake', user.id, (users: string[]) => {
+        setOnlineUsers(users);
+      });
+    }
+
     socket.on('user_connected', (user: string) => {
       console.log('User connected message received');
       setOnlineUsers((prev) => [...prev, user]);
@@ -37,29 +42,14 @@ const OnlineUsersProvider: FC<PropsWithChildren> = (props) => {
         prev.filter((onlineUserId) => onlineUserId !== uid)
       );
     });
-  }, [socket]);
-
-  useEffect(() => {
-    socket.connect();
-
-    // サーバーが保持するオンラインユーザー情報を取得し、状態を更新する
-    if (!didLogRef.current) {
-      didLogRef.current = true;
-      socket.emit('handshake', user.id, (users: string[]) => {
-        setOnlineUsers(users);
-      });
-    }
-
-    void startListeners();
 
     return () => {
-      console.log('unmount');
       socket.off('user_connected');
       socket.off('user_disconnected');
       socket.disconnect();
       socket.close();
     };
-  }, [startListeners, socket, user]);
+  }, [socket, user]);
 
   return (
     <OnlineUsersContext.Provider value={onlineUsers}>
