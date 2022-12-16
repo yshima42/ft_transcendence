df985c0fec6ced48134f124427154bd2ced84aec
yuki <shimazuyuki7@gmail.com>
2022-12-16 14:45:35 +0900

    refactor: フロントのball, paddleのpos削除

--- frontend/src/features/game/utils/gameObjs.ts
+++ frontend/src/features/game/utils/gameObjs.ts
@@ -7,28 +7,15 @@ import {
   PADDLE_WIDTH,
 } from './gameConfig';
 
-export class Vector {
+export class Ball {
   x: number;
   y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-
-  set(x: number, y: number): void {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-export class Ball {
-  pos: { x: number; y: number };
   dx: number;
   dy: number;
 
   constructor(x: number, y: number) {
-    this.pos = new Vector(x, y);
+    this.x = x;
+    this.y = y;
     this.dx = BALL_SPEED;
     this.dy = -BALL_SPEED;
   }
@@ -36,36 +23,38 @@ export class Ball {
   draw = (ctx: CanvasRenderingContext2D): void => {
     ctx.fillStyle = BALL_COLOR;
     ctx.beginPath();
-    ctx.arc(this.pos.x, this.pos.y, BALL_SIZE, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y, BALL_SIZE, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
   };
 
   setPosition = (x: number, y: number): void => {
-    this.pos.x = x;
-    this.pos.y = y;
+    this.x = x;
+    this.y = y;
   };
 }
 
 export class Paddle {
-  pos: { x: number; y: number };
+  x: number;
+  y: number;
   score: number;
 
   constructor(x: number, y: number) {
-    this.pos = new Vector(x, y);
+    this.x = x;
+    this.y = y;
     this.score = 0;
   }
 
   draw = (ctx: CanvasRenderingContext2D): void => {
     ctx.beginPath();
-    ctx.rect(this.pos.x, this.pos.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+    ctx.rect(this.x, this.y, PADDLE_WIDTH, PADDLE_HEIGHT);
     ctx.fillStyle = PADDLE_COLOR;
     ctx.fill();
     ctx.closePath();
   };
 
   setPosition = (x: number, y: number): void => {
-    this.pos.x = x;
-    this.pos.y = y;
+    this.x = x;
+    this.y = y;
   };
 }
