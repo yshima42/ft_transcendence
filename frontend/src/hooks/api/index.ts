53b45ca38021f62f6a8a92963be34c1182ab6667
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-12-16 14:33:01 +0900

    api/blockディレクトリ作成。

--- frontend/src/hooks/api/index.ts
+++ frontend/src/hooks/api/index.ts
@@ -8,14 +8,14 @@ export * from './profile/useAvatarUpload';
 export * from './profile/useProfile';
 
 export * from './relations/useAllUsers';
-export * from './relations/useBlockUsers';
+export * from './block/useBlockUsers';
 export * from './relations/useFriendRequest';
 export * from './relations/useFriendRequestAccept';
 export * from './relations/useFriendRequestCancel';
 export * from './relations/useFriendUnregister';
 export * from './relations/useFriends';
 export * from './relations/useIncomigUsers';
-export * from './relations/useIsBlockedUser';
+export * from './block/useIsBlockedUser';
 export * from './relations/useOutGoingUsers';
-export * from './relations/useUserBlock';
-export * from './relations/useUserBlockCancel';
+export * from './block/useUserBlock';
+export * from './block/useUserBlockCancel';
