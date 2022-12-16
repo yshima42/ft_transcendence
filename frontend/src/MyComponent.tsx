c3374a23be4aa5a847d2641a87aa0006a58d9c77
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-16 16:44:25 +0900

    フロントテスト仮置

---
+++ frontend/src/MyComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function MyComponent(): React.ReactElement {
+  const title = 'Hello Test';
+
+  return (
+    <>
+      <div>
+        <p>{title}</p>
+      </div>
+    </>
+  );
+}
