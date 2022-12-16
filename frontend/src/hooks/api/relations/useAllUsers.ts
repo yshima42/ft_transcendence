ccbceb7e9969c934e2b464310c87a563422bab8d
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-12-16 15:00:57 +0900

    useAllUsers使っていないので削除。

--- frontend/src/hooks/api/relations/useAllUsers.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@prisma/client';
-import { useGetApi } from '../generics/useGetApi';
-
-export const useAllUsers = (): { users: User[] } => {
-  const { data: users } = useGetApi<User[]>('/users/all');
-
-  return { users };
-};
