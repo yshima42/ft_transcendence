80404a112f916300a8ce1b02392ba9c854994c29
Masaya Kamei <km9smc6@gmail.com>
2022-12-19 16:48:21 +0900

    Matching 修正

--- frontend/src/features/game/hooks/useGameMatching.ts
+++ frontend/src/features/game/hooks/useGameMatching.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import { WS_BASE_URL } from 'config';
-import { useProfile } from 'hooks/api';
 import { useSocket } from 'hooks/socket/useSocket';
 import { useNavigate } from 'react-router-dom';
 
 export enum MatchState {
-  Matching = 0,
-  MatchingCancel = 1,
-  Matched = 2,
+  SocketConnecting = 0,
+  Matching = 1,
+  MatchingCancel = 2,
+  Matched = 3,
 }
 
 // ここでuseRefを使ってsocketのconnect処理ができたら理想
@@ -15,23 +15,28 @@ export const useGameMatching = (): {
   matchState: MatchState;
   setMatchState: React.Dispatch<React.SetStateAction<MatchState>>;
 } => {
-  const [matchState, setMatchState] = useState(MatchState.Matching);
+  const [matchState, setMatchState] = useState(MatchState.SocketConnecting);
   const socket = useSocket(`${WS_BASE_URL}/game`);
-
-  const { user } = useProfile();
   const navigate = useNavigate();
 
   // socket イベント
   useEffect(() => {
+    socket.on('connect_established', () => {
+      console.log('[Socket Event] connect_established');
+      setMatchState(MatchState.Matching);
+    });
+
     socket.on('go_game_room', (roomId: string, isLeftSide: boolean) => {
+      console.log('[Socket Event] go_game_room');
       setMatchState(MatchState.Matched);
-      navigate(`/games/${roomId}`, { state: { isLeftSide } });
+      navigate(`/app/games/${roomId}`, { state: { isLeftSide } });
     });
 
     return () => {
       if (matchState === MatchState.Matching) {
         socket.emit('matching_cancel');
       }
+      socket.off('connect_established');
       socket.off('go_game_room');
     };
   }, [socket, matchState, navigate]);
@@ -39,17 +44,18 @@ export const useGameMatching = (): {
   useEffect(() => {
     switch (matchState) {
       case MatchState.Matching: {
-        socket.emit('set_user', user);
+        console.log('[MatchState] Matching');
         socket.emit('random_match');
         break;
       }
       case MatchState.MatchingCancel: {
+        console.log('[MatchState] MatchingCancel');
         socket.emit('matching_cancel');
         navigate('/app');
         break;
       }
     }
-  }, [matchState, socket, user, navigate]);
+  }, [matchState, socket, navigate]);
 
   return { matchState, setMatchState };
 };
