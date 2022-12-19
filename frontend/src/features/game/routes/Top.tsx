104f9259c2fef1ec3f78c4b635dc6e88bfa38f0b
Masaya Kamei <km9smc6@gmail.com>
2022-12-19 12:02:21 +0900

    matching 部分作成

--- frontend/src/features/game/routes/Top.tsx
+++ frontend/src/features/game/routes/Top.tsx
@@ -1,37 +1,22 @@
-import { memo, FC, useMemo } from 'react';
+import { memo, FC } from 'react';
 import { Center } from '@chakra-ui/react';
 import { ContentLayout } from 'components/ecosystems/ContentLayout';
-import { Confirmation } from '../components/Confirmation';
 import { GameTop } from '../components/GameTop';
 import { Matching } from '../components/Matching';
-import { PongGame } from '../components/PongGame';
-import { Result } from '../components/Result';
-import { Waiting } from '../components/Waiting';
-import { GamePhase, useGame } from '../hooks/useGame';
+import { MatchState, useGameMatching } from '../hooks/useGameMatching';
 
 export const Top: FC = memo(() => {
-  const { gamePhase, setGamePhase, draw, gameResult } = useGame();
-
-  const gamePage = useMemo(() => {
-    switch (gamePhase) {
-      case GamePhase.Top:
-        return <GameTop setGamePhase={setGamePhase} />;
-      case GamePhase.Matching:
-        return <Matching />;
-      case GamePhase.Confirmation:
-        return <Confirmation setGamePhase={setGamePhase} />;
-      case GamePhase.Waiting:
-        return <Waiting />;
-      case GamePhase.InGame:
-        return <PongGame draw={draw} />;
-      case GamePhase.Result:
-        return <Result gameResult={gameResult} />;
-    }
-  }, [gamePhase, setGamePhase, draw]);
+  const { matchState, setMatchState } = useGameMatching();
 
   return (
     <ContentLayout title="">
-      <Center>{gamePage}</Center>
+      <Center>
+        {matchState === MatchState.None ? (
+          <GameTop setMatchState={setMatchState} />
+        ) : (
+          <Matching setMatchState={setMatchState} />
+        )}
+      </Center>
     </ContentLayout>
   );
 });
