ce7306195a01925da5b40a652452f250fcd4ce87
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-12-21 11:51:53 +0900

    index.tsを整理。

--- frontend/src/hooks/api/index.ts
+++ frontend/src/hooks/api/index.ts
@@ -1,20 +1,34 @@
+// auth
 export * from './auth/useDummyLogin';
+export * from './auth/useIsOtpAuthEnabled';
 export * from './auth/useLogout';
+export * from './auth/useOtpAuthCreate';
+export * from './auth/useOtpAuthDelete';
+export * from './auth/useOtpQrcodeUrl';
 
-export * from './game/useGameStats';
-export * from './game/useMatchHistory';
-
-export * from './profile/useAvatarUpload';
-export * from './profile/useProfile';
-
+// block
+export * from './block/useBlockRelation';
 export * from './block/useBlockUsers';
+export * from './block/useUserBlock';
+export * from './block/useUserBlockCancel';
+
+// friend
+export * from './friend/useFriendRelation';
 export * from './friend/useFriendRequest';
 export * from './friend/useFriendRequestAccept';
 export * from './friend/useFriendRequestCancel';
+export * from './friend/useFriendRequestReject';
 export * from './friend/useFriendUnregister';
 export * from './friend/useFriends';
 export * from './friend/useIncomigUsers';
-export * from './block/useBlockRelation';
 export * from './friend/useOutGoingUsers';
-export * from './block/useUserBlock';
-export * from './block/useUserBlockCancel';
+export * from './friend/useRequestableUsers';
+
+// game
+export * from './game/useGameStats';
+export * from './game/useMatchHistory';
+
+// profile
+export * from './profile/useAvatarUpload';
+export * from './profile/useProfile';
+export * from './profile/useProfileEdit';
