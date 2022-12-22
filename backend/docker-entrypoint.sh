c56ed5c63344dd20b722a87d78a6caa3e3ab8d3b
hyoshie_vm <dymn9.term@gmail.com>
2022-12-23 03:03:49 +0900

    seedを初回起動時のみ入れるように

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -eux
 # TODO: dbの起動をいい感じで待つ
 sleep 3
-yarn prisma generate
+# yarn prisma migrate deploy
 yarn prisma migrate dev --name init
-# yarn prisma db seed
+yarn prisma db seed
 yarn start:dev
