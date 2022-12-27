89868ee7016bdbddabae8bd5133680f2ff8ffa05
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-27 19:19:48 +0900

    refactor: ChatRoomMemberList

--- frontend/src/features/chat/components/organisms/ChatRoomMemberList.tsx
+++ frontend/src/features/chat/components/organisms/ChatRoomMemberList.tsx
@@ -22,95 +22,112 @@ export const ChatRoomMemberList: React.FC<Props> = React.memo(
   ({ chatLoginUser, chatMembers, changeChatRoomMemberStatus }) => {
     return (
       <C.List spacing={3}>
-        {chatMembers.map((user) => (
-          <C.ListItem key={user.user.id}>
-            <C.Flex>
-              <UserAvatar
-                id={user.user.id}
-                size="sm"
-                name={user.user.nickname}
-                src={user.user.avatarImageUrl}
-              ></UserAvatar>
-              <C.Text ml={10}>{user.user.nickname}</C.Text>
-              <C.Spacer />
-              <C.Flex>
-                <C.Text mr={5}>{user.memberStatus}</C.Text>
-                {chatLoginUser?.user.id === user.user.id && (
-                  <C.Flex>
-                    <C.Text mr={5}>me</C.Text>
-                  </C.Flex>
-                )}
-                {/* userがLoginUserでない、かつ、(LoginUserがADMIN または MODERATOR) かつ、userがNORMALのとき */}
-                {chatLoginUser !== undefined &&
-                  chatLoginUser.user.id !== user.user.id &&
-                  user.memberStatus === ChatRoomMemberStatus.NORMAL &&
-                  (chatLoginUser.memberStatus ===
-                    ChatRoomMemberStatus.MODERATOR ||
-                    chatLoginUser.memberStatus ===
-                      ChatRoomMemberStatus.ADMIN) && (
-                    <C.Flex>
-                      <ChangeChatRoomMemberStatusButtons
-                        userId={user.user.id}
-                        memberStatus={chatLoginUser.memberStatus}
-                        chatLoginUser={chatLoginUser}
-                        changeChatRoomMemberStatus={changeChatRoomMemberStatus}
-                      />
-                    </C.Flex>
-                  )}
-                {/* userがLoginUserでない、かつ、LoginUserがADMIN かつ、userがNORMALでないとき */}
-                {chatLoginUser !== undefined &&
-                  chatLoginUser.user.id !== user.user.id &&
-                  chatLoginUser.memberStatus === ChatRoomMemberStatus.ADMIN &&
-                  user.memberStatus !== ChatRoomMemberStatus.ADMIN &&
-                  user.memberStatus !== ChatRoomMemberStatus.NORMAL && (
-                    <C.Flex>
-                      <C.Button
-                        onClick={() =>
-                          changeChatRoomMemberStatus(
-                            user.user.id,
-                            ChatRoomMemberStatus.NORMAL,
-                            chatLoginUser
-                          )
-                        }
-                      >
-                        {
-                          changeChatRoomMemberStatusButtonTexts[
-                            user.memberStatus as ChatRoomMemberStatus
-                          ]
-                        }
-                      </C.Button>
-                    </C.Flex>
-                  )}
-                {chatLoginUser !== undefined &&
-                  chatLoginUser.user.id !== user.user.id &&
-                  chatLoginUser.memberStatus ===
-                    ChatRoomMemberStatus.MODERATOR &&
-                  user.memberStatus !== ChatRoomMemberStatus.MODERATOR &&
-                  user.memberStatus !== ChatRoomMemberStatus.ADMIN &&
-                  user.memberStatus !== ChatRoomMemberStatus.NORMAL && (
-                    <C.Flex>
-                      <C.Button
-                        onClick={() =>
-                          changeChatRoomMemberStatus(
-                            user.user.id,
-                            ChatRoomMemberStatus.NORMAL,
-                            chatLoginUser
-                          )
-                        }
-                      >
-                        {
-                          changeChatRoomMemberStatusButtonTexts[
-                            user.memberStatus as ChatRoomMemberStatus
-                          ]
-                        }
-                      </C.Button>
-                    </C.Flex>
-                  )}
-              </C.Flex>
-            </C.Flex>
-          </C.ListItem>
+        {chatMembers.map((member) => (
+          <ChatRoomMemberListItem
+            key={member.user.id}
+            member={member}
+            chatLoginUser={chatLoginUser}
+            changeChatRoomMemberStatus={changeChatRoomMemberStatus}
+          />
         ))}
       </C.List>
     );
   }
 );
+
+// ChatRoomMemberListItem
+const ChatRoomMemberListItem: React.FC<{
+  member: ResponseChatRoomMember;
+  chatLoginUser: ResponseChatRoomMember;
+  changeChatRoomMemberStatus: (
+    memberId: string,
+    memberStatus: ChatRoomMemberStatus,
+    chatLoginUser: ResponseChatRoomMember
+  ) => void;
+}> = React.memo(({ member, chatLoginUser, changeChatRoomMemberStatus }) => {
+  return (
+    <C.ListItem key={member.user.id}>
+      <C.Flex>
+        <UserAvatar
+          id={member.user.id}
+          size="sm"
+          name={member.user.nickname}
+          src={member.user.avatarImageUrl}
+        ></UserAvatar>
+        <C.Text ml={10}>{member.user.nickname}</C.Text>
+        <C.Spacer />
+        <C.Flex>
+          <C.Text mr={5}>{member.memberStatus}</C.Text>
+          {chatLoginUser?.user.id === member.user.id && (
+            <C.Flex>
+              <C.Text mr={5}>me</C.Text>
+            </C.Flex>
+          )}
+          {/* userがLoginUserでない、かつ、(LoginUserがADMIN または MODERATOR) かつ、userがNORMALのとき */}
+          {chatLoginUser !== undefined &&
+            chatLoginUser.user.id !== member.user.id &&
+            member.memberStatus === ChatRoomMemberStatus.NORMAL &&
+            (chatLoginUser.memberStatus === ChatRoomMemberStatus.MODERATOR ||
+              chatLoginUser.memberStatus === ChatRoomMemberStatus.ADMIN) && (
+              <C.Flex>
+                <ChangeChatRoomMemberStatusButtons
+                  userId={member.user.id}
+                  memberStatus={chatLoginUser.memberStatus}
+                  chatLoginUser={chatLoginUser}
+                  changeChatRoomMemberStatus={changeChatRoomMemberStatus}
+                />
+              </C.Flex>
+            )}
+          {/* memberがLoginUserでない、かつ、LoginUserがADMIN かつ、userがNORMALでないとき */}
+          {chatLoginUser !== undefined &&
+            chatLoginUser.user.id !== member.user.id &&
+            chatLoginUser.memberStatus === ChatRoomMemberStatus.ADMIN &&
+            member.memberStatus !== ChatRoomMemberStatus.ADMIN &&
+            member.memberStatus !== ChatRoomMemberStatus.NORMAL && (
+              <C.Flex>
+                <C.Button
+                  onClick={() =>
+                    changeChatRoomMemberStatus(
+                      member.user.id,
+                      ChatRoomMemberStatus.NORMAL,
+                      chatLoginUser
+                    )
+                  }
+                >
+                  {
+                    changeChatRoomMemberStatusButtonTexts[
+                      member.memberStatus as ChatRoomMemberStatus
+                    ]
+                  }
+                </C.Button>
+              </C.Flex>
+            )}
+          {chatLoginUser !== undefined &&
+            chatLoginUser.user.id !== member.user.id &&
+            chatLoginUser.memberStatus === ChatRoomMemberStatus.MODERATOR &&
+            member.memberStatus !== ChatRoomMemberStatus.MODERATOR &&
+            member.memberStatus !== ChatRoomMemberStatus.ADMIN &&
+            member.memberStatus !== ChatRoomMemberStatus.NORMAL && (
+              <C.Flex>
+                <C.Button
+                  onClick={() =>
+                    changeChatRoomMemberStatus(
+                      member.user.id,
+                      ChatRoomMemberStatus.NORMAL,
+                      chatLoginUser
+                    )
+                  }
+                >
+                  {
+                    changeChatRoomMemberStatusButtonTexts[
+                      member.memberStatus as ChatRoomMemberStatus
+                    ]
+                  }
+                </C.Button>
+              </C.Flex>
+            )}
+        </C.Flex>
+      </C.Flex>
+    </C.ListItem>
+  );
+});
