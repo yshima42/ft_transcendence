f2620b1540d93d5d7a1d91030f85d9e84197e086
Masaya Kamei <km9smc6@gmail.com>
2022-12-27 14:24:27 +0900

    ゲーム中画面にavatarwithnickname 追加

--- frontend/src/features/game/components/PongGame.tsx
+++ frontend/src/features/game/components/PongGame.tsx
@@ -1,14 +1,29 @@
 import { memo, FC } from 'react';
+import { Flex } from '@chakra-ui/react';
+import { useProfile } from 'hooks/api';
 import { useCanvas } from '../hooks/useCanvas';
+import { Player } from '../hooks/useGame';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../utils/gameConfig';
+import { AvatarWithNickname } from './AvatarWithNickname';
 
 type Props = {
   draw: (ctx: CanvasRenderingContext2D) => void;
+  player1: Player;
+  player2: Player;
 };
 
 export const PongGame: FC<Props> = memo((props) => {
-  const { draw } = props;
-  const canvasRef = useCanvas(draw);
+  const { draw, player1, player2 } = props;
 
-  return <canvas width={CANVAS_WIDTH} height={CANVAS_HEIGHT} ref={canvasRef} />;
+  const canvasRef = useCanvas(draw);
+  const { user: user1 } = useProfile(player1.id);
+  const { user: user2 } = useProfile(player2.id);
+
+  return (
+    <Flex alignItems="center" p={4}>
+      <AvatarWithNickname user={user1} />
+      <canvas width={CANVAS_WIDTH} height={CANVAS_HEIGHT} ref={canvasRef} />
+      <AvatarWithNickname user={user2} />
+    </Flex>
+  );
 });
