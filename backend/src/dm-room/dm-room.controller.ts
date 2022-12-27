3475267499203af3d6527feeed40e83f421725ca
kohkubo <kohkubo@student.42tokyo.jp>
2022-12-27 14:32:02 +0900

    ブロックしている場合に処理しないように変更

--- backend/src/dm-room/dm-room.controller.ts
+++ backend/src/dm-room/dm-room.controller.ts
@@ -5,11 +5,14 @@ import {
   Param,
   ParseUUIDPipe,
   UseGuards,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import * as Sw from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { JwtOtpAuthGuard } from 'src/auth/guards/jwt-otp-auth.guard';
+import { FriendRequestsService } from 'src/friend-requests/friend-requests.service';
 import { ResponseDmRoom } from './dm-room.interface';
 import { DmRoomService } from './dm-room.service';
 
@@ -17,13 +20,28 @@ import { DmRoomService } from './dm-room.service';
 @Sw.ApiTags('dm-room')
 @UseGuards(JwtOtpAuthGuard)
 export class DmRoomController {
-  constructor(private readonly dmRoomService: DmRoomService) {}
+  constructor(
+    private readonly dmRoomService: DmRoomService,
+    private readonly friendRequestsService: FriendRequestsService
+  ) {}
 
   @Post(':userId')
   async findOneOrCreate(
     @Param('userId', new ParseUUIDPipe()) userId: string,
     @GetUser() user: User
   ): Promise<string> {
+    if (userId === user.id) {
+      throw new HttpException(
+        'you cannot create dm room with yourself',
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    if (await this.friendRequestsService.isBlockRelation(user.id, userId)) {
+      throw new HttpException(
+        'you cannot create dm room with block relation',
+        HttpStatus.BAD_REQUEST
+      );
+    }
     const res = await this.dmRoomService.findOrCreate(userId, user.id);
 
     return res.id;
@@ -31,6 +49,6 @@ export class DmRoomController {
 
   @Get()
   async findAll(@GetUser() user: User): Promise<ResponseDmRoom[]> {
-    return await this.dmRoomService.findAll(user.id);
+    return await this.dmRoomService.findAllWithoutBlockUser(user.id);
   }
 }
--- backend/src/dm-room/dm-room.module.ts
+++ backend/src/dm-room/dm-room.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { FriendRequestsModule } from 'src/friend-requests/friend-requests.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { DmRoomController } from './dm-room.controller';
 import { DmRoomService } from './dm-room.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, FriendRequestsModule],
   controllers: [DmRoomController],
   providers: [DmRoomService],
 })
