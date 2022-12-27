eeb0c8c68748e82a6a16d0080bf32fb1a7e2481c
Masaya Kamei <km9smc6@gmail.com>
2022-12-27 13:59:19 +0900

    gameResult アバター表示

--- frontend/src/features/game/components/Result.tsx
+++ frontend/src/features/game/components/Result.tsx
@@ -1,7 +1,18 @@
 import { memo, FC } from 'react';
-import { Box, Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Spacer,
+  Stack,
+  VStack,
+} from '@chakra-ui/react';
 import { useProfile } from 'hooks/api';
 import { useNavigate } from 'react-router-dom';
+import { LinkedNickname } from 'components/atoms/text/LinkedNickname';
+import { UserAvatar } from 'components/organisms/avatar/UserAvatar';
 import { Player } from '../hooks/useGame';
 
 type Props = {
@@ -14,23 +25,38 @@ export const Result: FC<Props> = memo((props) => {
 
   const { user: user1 } = useProfile(player1.id);
   const { user: user2 } = useProfile(player2.id);
-  const [player1Nickname, player2Nickname] = [user1.nickname, user2.nickname];
   const navigate = useNavigate();
 
-  // TODO: Winnerの表示実装。Winnerをバック、フロントどちらで実装するか検討
-  // TODO: Top画面への遷移実装。現在ゲームページがappで行われているため動かない。
   return (
     <>
       <Flex align="center" justify="center" height="40vh">
-        <Box bg="white" w="md" p={4} borderRadius="md" shadow="md">
-          <Heading as="h2" size="md" textAlign="center">
-            {player1Nickname} vs {player2Nickname}
-          </Heading>
-          <Heading as="h2" size="lg" textAlign="center">
-            {player1.score} - {player2.score}
-          </Heading>
+        <Box bg="white" w="sz" p={4} borderRadius="md" shadow="md">
+          <Flex alignItems="center" p={4}>
+            <Spacer />
+            <VStack alignItems={'center'}>
+              <UserAvatar id={user1.id} src={user1.avatarImageUrl} />
+              <LinkedNickname
+                id={user1.id}
+                nickname={user1.nickname}
+                maxWidth={200}
+              />
+            </VStack>
+            <Spacer />
+            <Heading as="h2" size="lg" textAlign="center">
+              {player1.score} - {player2.score}
+            </Heading>
+            <Spacer />
+            <VStack alignItems={'center'}>
+              <UserAvatar id={user2.id} src={user2.avatarImageUrl} />
+              <LinkedNickname
+                id={user2.id}
+                nickname={user2.nickname}
+                maxWidth={200}
+              />
+            </VStack>
+          </Flex>
           <Divider />
-          <Stack spacing={4} py={4} px={10} align="center">
+          <Stack py={3} align="center">
             <Button
               onClick={() => {
                 navigate('/app');
