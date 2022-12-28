d5c802b9baf893bb0f052a14cffaaaeecea0cd3c
hyoshie_vm <dymn9.term@gmail.com>
2022-12-28 19:21:43 +0900

    fix: ２回目以降のdocker-composeでmigrateが失敗するバグを修正

--- backend/setup-dev.sh
+++ backend/setup-dev.sh
@@ -20,7 +20,6 @@ if [ ! -f .env ]; then
   echo -e "${RED}No .env file ｡ﾟ(ﾟ´Д｀ﾟ)ﾟ｡${RESET}"
   exit 1
 fi
-rm -rf "prisma/migrations"
 yarn install
 yarn prisma generate # prismaの型定義を生成
 yarn prisma migrate dev --name init
