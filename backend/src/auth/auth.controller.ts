56624b4806f36318af03e371868a5a5889dae739
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-12-28 23:05:17 +0900

    userが必ずotpAuthレコードを持つように修正。その他修正。

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -8,8 +8,6 @@ import {
   Res,
   Redirect,
   Query,
-  Delete,
-  UnauthorizedException,
   Body,
   Patch,
 } from '@nestjs/common';
@@ -24,6 +22,7 @@ import { FtOauthGuard } from './guards/ft-oauth.guard';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { JwtOtpAuthGuard } from './guards/jwt-otp-auth.guard';
 import { FtProfile } from './interfaces/ft-profile.interface';
+import { OneTimePasswordAuthResponse } from './interfaces/otp-auth-response.interface';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -54,6 +53,9 @@ export class AuthController {
       name,
       nickname: name,
       avatarImageUrl: ftProfile.imageUrl,
+      oneTimePasswordAuth: {
+        create: {},
+      },
     };
 
     const { accessToken, isOtpAuthEnabled, isSignUp } =
@@ -66,7 +68,7 @@ export class AuthController {
 
     if (isSignUp) {
       return { url: 'http://localhost:5173/sign-up' };
-    } else if (isOtpAuthEnabled !== null && isOtpAuthEnabled) {
+    } else if (isOtpAuthEnabled) {
       return { url: 'http://localhost:5173/otp' };
     } else {
       return { url: 'http://localhost:5173/app' };
@@ -100,7 +102,7 @@ export class AuthController {
     );
     res.cookie('accessToken', accessToken, this.cookieOptions);
 
-    if (isOtpAuthEnabled !== null && isOtpAuthEnabled) {
+    if (isOtpAuthEnabled) {
       return { url: 'http://localhost:5173/otp' };
     } else {
       return { url: 'http://localhost:5173/app' };
@@ -117,144 +119,74 @@ export class AuthController {
   }
 
   /**
-   * OneTimePasswordAuthテーブルに新規レコードを追加。
-   * @param user - 対象ユーザー
-   * @param res - cookie用
-   * @returns message
+   * OneTimePassword Auth
    */
-  @Post('otp')
-  @UseGuards(JwtOtpAuthGuard)
-  async createOtpAuth(
-    @GetUser() user: User,
-    @Res({ passthrough: true }) res: Response
-  ): Promise<{ message: string }> {
-    await this.authService.createOtpAuth(user);
 
-    const { accessToken } = await this.authService.generateJwt(
-      user.id,
-      user.name,
-      true
-    );
-
-    res.cookie('accessToken', accessToken, this.cookieOptions);
-
-    return { message: 'ok' };
-  }
-
-  /**
-   * OneTimePasswordAuthテーブルからレコード削除。
-   * @param user - 対象ユーザー
-   * @param res - cookie用
-   * @returns message
-   */
-  @Delete('otp')
-  @UseGuards(JwtOtpAuthGuard)
-  async deleteOtpAuth(
-    @GetUser() user: User,
-    @Res({ passthrough: true }) res: Response
-  ): Promise<{ message: string }> {
-    await this.authService.deleteOtpAuth(user);
-
-    const { accessToken } = await this.authService.generateJwt(
-      user.id,
-      user.name,
-      false
-    );
-
-    res.cookie('accessToken', accessToken, this.cookieOptions);
-
-    return { message: 'ok' };
-  }
-
-  /**
-   * 対象ユーザーのワンタイムパスワード生成用QRコードのURLを返す。
-   * @param user
-   * @returns QRコードのURL
-   */
-  @Get('otp/qrcode-url')
-  @UseGuards(JwtOtpAuthGuard)
-  async getOtpQrcodeUrl(@GetUser() user: User): Promise<{ qrcodeUrl: string }> {
-    return await this.authService.getOtpQrcodeUrl(user);
-  }
-
-  /**
-   * OneTimePasswordAuthテーブル上に、特定のユーザーのレコードが存在するかどうか確認。
-   * また、存在してる場合、isOtpAuthEnabledプロパティがtrueかどうかを確認。
-   * @param user
-   * @returns レコードが存在しない場合null。レコードが存在する場合bool値。
-   */
   @Get('otp')
   @UseGuards(JwtOtpAuthGuard)
-  async isOtpAuthEnabled(
+  async findOtpAuth(
     @GetUser() user: User
-  ): Promise<{ isOtpAuthEnabled: boolean | null }> {
-    return await this.authService.isOtpAuthEnabled(user.id);
+  ): Promise<OneTimePasswordAuthResponse> {
+    return await this.authService.findOtpAuth(user.id);
   }
 
-  @Patch('otp')
+  @Post('otp')
+  @UseGuards(JwtOtpAuthGuard)
+  async createOtpAuthQrcodeUrl(
+    @GetUser() user: User
+  ): Promise<OneTimePasswordAuthResponse> {
+    return await this.authService.createOtpAuthQrcodeUrl(user);
+  }
+
+  @Patch('otp/on')
   @HttpCode(200)
   @UseGuards(JwtAuthGuard)
   async activateOtp(
     @GetUser() user: User,
     @Body('oneTimePassword') oneTimePassword: string,
     @Res({ passthrough: true }) res: Response
-  ): Promise<{ message: string }> {
-    const isCodeValid = await this.authService.validateOtp(
-      oneTimePassword,
+  ): Promise<OneTimePasswordAuthResponse> {
+    const { otpAuthResponse, accessToken } = await this.authService.activeOtp(
+      user,
+      oneTimePassword
+    );
+
+    res.cookie('accessToken', accessToken, this.cookieOptions);
+
+    return otpAuthResponse;
+  }
+
+  @Patch('otp/off')
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  async inactivateOtp(
+    @GetUser() user: User,
+    @Res({ passthrough: true }) res: Response
+  ): Promise<OneTimePasswordAuthResponse> {
+    const { otpAuthResponse, accessToken } = await this.authService.inactiveOtp(
       user
     );
-    if (!isCodeValid) {
-      throw new UnauthorizedException('Wrong authentication code');
-    }
 
-    await this.authService.activateOtp(user);
+    res.cookie('accessToken', accessToken, this.cookieOptions);
 
-    const { accessToken } = await this.authService.generateJwt(
-      user.id,
-      user.name,
-      true
+    return otpAuthResponse;
+  }
+
+  @Post('otp/validation')
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  async validateOtp(
+    @GetUser() user: User,
+    @Body('oneTimePassword') oneTimePassword: string,
+    @Res({ passthrough: true }) res: Response
+  ): Promise<{ message: string }> {
+    const { accessToken } = await this.authService.validateOtp(
+      oneTimePassword,
+      user
     );
 
     res.cookie('accessToken', accessToken, this.cookieOptions);
 
     return { message: 'ok' };
-  }
-
-  /**
-   * 入力されたワンタイムパスワードの検証。
-   * 正しければ、valid=trueを付与したaccessTokenを
-   * cookieに割り当てて、アプリトップへリダイレクト。
-   * 間違っていれば、ログインページにリダイレクト。
-   * @param user
-   * @param oneTimePassword - クエリから取得。
-   * @param res - cookie用
-   * @returns リダイレクト先
-   */
-  @Get('otp/validation')
-  @HttpCode(200)
-  @Redirect('http://localhost:5173/app')
-  @UseGuards(JwtAuthGuard)
-  async validateOtp(
-    @GetUser() user: User,
-    @Query('one-time-password') oneTimePassword: string,
-    @Res({ passthrough: true }) res: Response
-  ): Promise<{ url: string }> {
-    const isCodeValid = await this.authService.validateOtp(
-      oneTimePassword,
-      user
-    );
-    if (!isCodeValid) {
-      return { url: 'http://localhost:5173/' };
-    }
-
-    const { accessToken } = await this.authService.generateJwt(
-      user.id,
-      user.name,
-      true
-    );
-
-    res.cookie('accessToken', accessToken, this.cookieOptions);
-
-    return { url: 'http://localhost:5173/app' };
   }
 }
