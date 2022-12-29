c1bb0605b296fe136c6800e5fc5d8eb783b1dfec
hyoshie_vm <dymn9.term@gmail.com>
2022-12-29 12:49:01 +0900

    chore: apt install errorに対応

--- setup-42vm.sh
+++ setup-42vm.sh
@@ -2,6 +2,11 @@
 
 set -eux
 
+# Case: E: Unable to acquire the dpkg frontend lock (/var/lib/dpkg/lock-frontend), is another process using it?
+# sudo rm /var/lib/apt/lists/lock
+# sudo rm /var/lib/dpkg/lock
+# sudo rm /var/lib/dpkg/lock-frontend
+
 # Update Browser
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt update
