01603d78265fa279af76ad7677047023ff57a4c7
yuki <shimazuyuki7@gmail.com>
2023-01-04 16:42:05 +0900

    configをbackend, frontend共通化

--- backend/src/game/config/game-config.ts
+++ backend/src/game/config/game-config.ts
@@ -1,5 +1,3 @@
-// TODO: これらを設定などで変えられる機能を作る
-// TODO: フロントと共通にしないといけない
 export const CANVAS_WIDTH = 780;
 export const CANVAS_HEIGHT = 540;
 export const BALL_START_X = CANVAS_WIDTH / 2;
@@ -11,3 +9,5 @@ export const PADDLE_HEIGHT = 100;
 export const PADDLE_START_POS = (CANVAS_HEIGHT - PADDLE_HEIGHT) / 2;
 export const PADDLE_SPEED = 32;
 export const SCORE_TO_WIN = 5;
+export const OBJS_COLOR = '#4FD1C5';
+export const BG_COLOR = '#1A202C';
