101e9f16df38f10615d7772b9c893e590c08cbc6
yuki <shimazuyuki7@gmail.com>
2023-01-04 14:32:45 +0900

    chore: ballサイズ、paddleサイズレスポンシブに

--- frontend/src/features/game/utils/gameObjs.ts
+++ frontend/src/features/game/utils/gameObjs.ts
@@ -9,17 +9,19 @@ import {
 export class Ball {
   x: number;
   y: number;
+  size: number;
 
   // 位置はサーバーから取ってくるため初期値必要なし
   constructor() {
     this.x = 0;
     this.y = 0;
+    this.size = BALL_SIZE;
   }
 
   draw = (ctx: CanvasRenderingContext2D): void => {
     ctx.fillStyle = BALL_COLOR;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, BALL_SIZE, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
   };
@@ -33,15 +35,19 @@ export class Ball {
 export class Paddle {
   x: number;
   y: number;
+  width: number;
+  height: number;
 
   constructor() {
     this.x = 0;
     this.y = 0;
+    this.width = PADDLE_WIDTH;
+    this.height = PADDLE_HEIGHT;
   }
 
   draw = (ctx: CanvasRenderingContext2D): void => {
     ctx.beginPath();
-    ctx.rect(this.x, this.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+    ctx.rect(this.x, this.y, this.width, this.height);
     ctx.fillStyle = PADDLE_COLOR;
     ctx.fill();
     ctx.closePath();
