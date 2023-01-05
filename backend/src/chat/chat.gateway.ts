157f6aa47e3d670ba1aa4b7a58ff327dd6cf8394
kohkubo <kohkubo@student.42tokyo.jp>
2023-01-06 00:39:18 +0900

    gateway側でclientIdとuserIdの紐付けを消去

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -22,81 +22,12 @@ export class ChatGateway {
     private readonly jwt: JwtService
   ) {}
 
-  // userIdとclientを紐付けるためのMap userId -> client[]
-  private readonly userIdToClientMap = new Map<string, string[]>();
-
   @WebSocket.WebSocketServer()
   server!: SocketIO.Server;
 
   // Logger関数
   private readonly logger = new NestJs.Logger('ChatGateway');
   private readonly json = (obj: any): string => JSON.stringify(obj, null, 2);
-
-  // userIdとclientを紐付ける関数
-  private addClientToUserIdMap(userId: string, client: string): void {
-    if (this.userIdToClientMap.has(userId)) {
-      const clients = this.userIdToClientMap.get(userId);
-      // すでに紐付けられている場合は、clientを追加する
-      if (clients !== undefined) {
-        clients.push(client);
-      }
-    } else {
-      // まだ紐付けられていない場合は、新規にMapを作成する
-      this.userIdToClientMap.set(userId, [client]);
-    }
-  }
-
-  // userIdとclientの紐付けを解除する関数
-  private removeClientFromUserIdMap(userId: string, client: string): void {
-    if (this.userIdToClientMap.has(userId)) {
-      const clients = this.userIdToClientMap.get(userId);
-      if (clients !== undefined) {
-        // clientを削除する
-        const index = clients.indexOf(client);
-        if (index > -1) {
-          clients.splice(index, 1);
-        }
-        // clientがなくなったら、Mapから削除する
-        if (clients.length === 0) {
-          this.userIdToClientMap.delete(userId);
-        }
-      }
-    }
-  }
-
-  // userIdとclientを紐付ける
-  handleConnection(@WebSocket.ConnectedSocket() client: SocketIO.Socket): void {
-    // mapを全部表示
-    this.logger.debug(`handleConnection: ${this.json(this.userIdToClientMap)}`);
-    const cookie = client.handshake.headers.cookie;
-    if (cookie === undefined) {
-      this.logger.warn('cookie is undefined');
-
-      return;
-    }
-    const chatLoginUserId = this.getUserIdFromCookie(cookie);
-    this.logger.debug(`handleConnection: ${chatLoginUserId}`);
-    // userIdとclientを紐付ける
-    this.addClientToUserIdMap(chatLoginUserId, client.id);
-    // clientにuserIdを紐付ける
-    client.data.userId = chatLoginUserId;
-  }
-
-  // userIdとclientの紐付けを解除する
-  handleDisconnect(@WebSocket.ConnectedSocket() client: SocketIO.Socket): void {
-    const cookie = client.handshake.headers.cookie;
-    if (cookie === undefined) {
-      this.logger.warn('cookie is undefined');
-
-      return;
-    }
-    const chatLoginUserId = this.getUserIdFromCookie(cookie);
-    this.logger.debug(`handleDisconnect: ${chatLoginUserId}`);
-    // userIdとclientを紐付けを解除する
-    this.removeClientFromUserIdMap(chatLoginUserId, client.id);
-    // clientからuserIdを削除する
-    delete client.data.userId;
-  }
 
   @NestJs.UsePipes(new NestJs.ValidationPipe())
   @WebSocket.SubscribeMessage('send_message')
@@ -145,7 +76,6 @@ export class ChatGateway {
   }
 
   // ユーザーのステータス変更
-  // TODO: 入力のバリデーション ID要素をPipeでチェックしたい。
   @NestJs.UsePipes(new NestJs.ValidationPipe())
   @WebSocket.SubscribeMessage('changeChatRoomMemberStatusSocket')
   async changeStatus(
