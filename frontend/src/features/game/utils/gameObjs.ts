006549cd2a6bcac76e4ac22f37684cd0755ffc55
Masaya Kamei <km9smc6@gmail.com>
2023-01-05 15:40:27 +0900

    ratioとdraw 調整

--- frontend/src/features/game/utils/gameObjs.ts
+++ frontend/src/features/game/utils/gameObjs.ts
@@ -3,6 +3,7 @@ import {
   BALL_SIZE,
   PADDLE_HEIGHT,
   PADDLE_WIDTH,
+  CANVAS_WIDTH,
 } from './gameConfig';
 
 export class Ball {
@@ -17,10 +18,10 @@ export class Ball {
     this.size = BALL_SIZE;
   }
 
-  draw = (ctx: CanvasRenderingContext2D): void => {
+  draw = (ctx: CanvasRenderingContext2D, ratio: number): void => {
     ctx.fillStyle = OBJS_COLOR;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.arc(this.x * ratio, this.y * ratio, this.size * ratio, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
   };
@@ -44,9 +45,14 @@ export class Paddle {
     this.height = PADDLE_HEIGHT;
   }
 
-  draw = (ctx: CanvasRenderingContext2D): void => {
+  draw = (ctx: CanvasRenderingContext2D, ratio: number): void => {
     ctx.beginPath();
-    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.rect(
+      this.x * ratio,
+      this.y * ratio,
+      this.width * ratio,
+      this.height * ratio
+    );
     ctx.fillStyle = OBJS_COLOR;
     ctx.fill();
     ctx.closePath();
@@ -66,4 +72,18 @@ export class Player {
     this.id = '';
     this.score = 0;
   }
+
+  drawScore = (
+    ctx: CanvasRenderingContext2D,
+    ratio: number,
+    isLeftSide: boolean
+  ): void => {
+    const fontSize = 48 * ratio;
+    ctx.font = `${fontSize}px serif`;
+    const [x, y] = isLeftSide
+      ? [20 * ratio, 50 * ratio]
+      : [(CANVAS_WIDTH - 40) * ratio, 50 * ratio];
+
+    ctx.fillText(this.score.toString(), x, y);
+  };
 }
