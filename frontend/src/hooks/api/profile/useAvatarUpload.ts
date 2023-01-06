e97cdaeb5ff905fd927f686c6e1d7433f2170df1
Masaya Kamei <km9smc6@gmail.com>
2023-01-06 10:58:02 +0900

    api/profile 修正

--- frontend/src/hooks/api/profile/useAvatarUpload.ts
+++ frontend/src/hooks/api/profile/useAvatarUpload.ts
@@ -1,6 +1,9 @@
-import { useEffect } from 'react';
 import { User } from '@prisma/client';
-import { UseMutateAsyncFunction } from '@tanstack/react-query';
+import {
+  UseMutateAsyncFunction,
+  UseMutationResult,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { isAxiosError } from 'axios';
 import { useCustomToast } from 'hooks/utils/useCustomToast';
 import { usePostFileApi } from '../generics/usePostFileApi';
@@ -20,28 +23,29 @@ export type UploadAvatar = UseMutateAsyncFunction<
   unknown
 >;
 
-export const useAvatarUpload = (): {
+export const useAvatarUpload = (): Omit<
+  UseMutationResult<AvatarUploadResBody, unknown, AvatarFormData, unknown>,
+  'mutateAsync'
+> & {
   uploadAvatar: UploadAvatar;
-  isLoading: boolean;
-  isSuccess: boolean;
 } => {
-  const {
-    mutateAsync: uploadAvatar,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-  } = usePostFileApi<AvatarUploadResBody>('/users/me/avatar', [
-    ['/users/me/profile'],
-    ['/game/matches'],
-  ]);
-
+  const queryClient = useQueryClient();
   const { customToast } = useCustomToast();
-  useEffect(() => {
-    if (isError && isAxiosError<{ message: string }>(error)) {
-      customToast({ description: error.response?.data.message });
-    }
-  }, [isError, error, customToast]);
 
-  return { uploadAvatar, isLoading, isSuccess };
+  const { mutateAsync: uploadAvatar, ...useMutationResult } =
+    usePostFileApi<AvatarUploadResBody>('/users/me/avatar', {
+      onSuccess: () => {
+        const queryKeys = [['/users/me/profile'], ['/game/matches']];
+        queryKeys.forEach((queryKey) => {
+          void queryClient.invalidateQueries({ queryKey });
+        });
+      },
+      onError: (error) => {
+        if (isAxiosError<{ message: string }>(error)) {
+          customToast({ description: error.response?.data.message });
+        }
+      },
+    });
+
+  return { uploadAvatar, ...useMutationResult };
 };
