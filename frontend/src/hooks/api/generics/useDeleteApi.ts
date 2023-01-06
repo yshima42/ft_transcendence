eaa4d47d2d6ff2dcbb03910d2ba2dc44939653a3
Masaya Kamei <km9smc6@gmail.com>
2023-01-06 10:51:56 +0900

    更新系generices 修正

--- frontend/src/hooks/api/generics/useDeleteApi.ts
+++ frontend/src/hooks/api/generics/useDeleteApi.ts
@@ -1,15 +1,12 @@
 import {
-  QueryKey,
   useMutation,
   UseMutationOptions,
   UseMutationResult,
-  useQueryClient,
 } from '@tanstack/react-query';
 import { axios } from '../../../lib/axios';
 
 export function useDeleteApi<ResBody>(
   endpoint: string,
-  queryKeys?: QueryKey[],
   useMutationOptions?: UseMutationOptions<ResBody, unknown, void, unknown>
 ): UseMutationResult<ResBody, unknown, void, unknown> {
   const axiosDelete = async () => {
@@ -18,18 +15,5 @@ export function useDeleteApi<ResBody>(
     return result.data;
   };
 
-  const queryClient = useQueryClient();
-
-  const useMutationResult = useMutation(axiosDelete, {
-    onSuccess: () => {
-      if (queryKeys !== undefined) {
-        queryKeys.forEach((queryKey) => {
-          void queryClient.invalidateQueries({ queryKey });
-        });
-      }
-    },
-    ...useMutationOptions,
-  });
-
-  return useMutationResult;
+  return useMutation(axiosDelete, useMutationOptions);
 }
