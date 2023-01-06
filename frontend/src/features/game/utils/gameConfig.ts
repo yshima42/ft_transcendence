c476cee5acdf79acd6088c0fe5fe918731d41c4d
yuki <shimazuyuki7@gmail.com>
2023-01-06 12:01:00 +0900

    refactor: ハードコーディング修正

--- frontend/src/features/game/utils/gameConfig.ts
+++ frontend/src/features/game/utils/gameConfig.ts
@@ -9,5 +9,6 @@ export const PADDLE_HEIGHT = 100;
 export const PADDLE_START_POS = (CANVAS_HEIGHT - PADDLE_HEIGHT) / 2;
 export const PADDLE_SPEED = 32;
 export const SCORE_TO_WIN = 5;
+export const READY_COUNTDOWN_NUM = 10;
 export const OBJS_COLOR = '#4FD1C5';
 export const BG_COLOR = '#1A202C';
