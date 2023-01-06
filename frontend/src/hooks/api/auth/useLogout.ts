e5c025effa44ee603d8a533a0fcdb0e80432b8ed
Masaya Kamei <km9smc6@gmail.com>
2023-01-06 12:08:20 +0900

    api/auth 修正

--- frontend/src/hooks/api/auth/useLogout.ts
+++ frontend/src/hooks/api/auth/useLogout.ts
@@ -1,5 +1,7 @@
-import { useEffect } from 'react';
-import { UseMutateAsyncFunction } from '@tanstack/react-query';
+import {
+  UseMutateAsyncFunction,
+  UseMutationResult,
+} from '@tanstack/react-query';
 import { isAxiosError } from 'axios';
 import { useCustomToast } from 'hooks/utils/useCustomToast';
 import { usePostApi } from '../generics/usePostApi';
@@ -17,25 +19,24 @@ export type Logout = UseMutateAsyncFunction<
   unknown
 >;
 
-export const useLogout = (): {
+export const useLogout = (): Omit<
+  UseMutationResult<LogoutResBody, unknown, LogoutReqBody, unknown>,
+  'mutateAsync'
+> & {
   logout: Logout;
-  isLoading: boolean;
-  isSuccess: boolean;
 } => {
-  const {
-    mutateAsync: logout,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-  } = usePostApi<LogoutReqBody, LogoutResBody>('/auth/logout');
-
   const { customToast } = useCustomToast();
-  useEffect(() => {
-    if (isError && isAxiosError<{ message: string }>(error)) {
-      customToast({ description: error.response?.data.message });
-    }
-  }, [isError, error, customToast]);
 
-  return { logout, isLoading, isSuccess };
+  const { mutateAsync: logout, ...useMutationResult } = usePostApi<
+    LogoutReqBody,
+    LogoutResBody
+  >('/auth/logout', {
+    onError: (error) => {
+      if (isAxiosError<{ message: string }>(error)) {
+        customToast({ description: error.response?.data.message });
+      }
+    },
+  });
+
+  return { logout, ...useMutationResult };
 };
--- frontend/src/hooks/api/auth/useOtpQrcodeCreate.ts
+++ frontend/src/hooks/api/auth/useOtpQrcodeCreate.ts
@@ -1,5 +1,8 @@
-import { useEffect } from 'react';
-import { UseMutateAsyncFunction } from '@tanstack/react-query';
+import {
+  UseMutateAsyncFunction,
+  UseMutationResult,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { isAxiosError } from 'axios';
 import { useCustomToast } from 'hooks/utils/useCustomToast';
 import { usePostApi } from '../generics/usePostApi';
@@ -18,28 +21,37 @@ export type CreateOtpAuthQrcodeUrl = UseMutateAsyncFunction<
   unknown
 >;
 
-export const useOtpAuthQrcodeCreate = (): {
+export const useOtpAuthQrcodeCreate = (): Omit<
+  UseMutationResult<
+    CreateOtpAuthQrcodeUrlResBody,
+    unknown,
+    CreateOtpAuthQrcodeUrlReqBody,
+    unknown
+  >,
+  'mutateAsync'
+> & {
   createOtpAuthQrcodeUrl: CreateOtpAuthQrcodeUrl;
-  isLoading: boolean;
-  isSuccess: boolean;
 } => {
-  const {
-    mutateAsync: createOtpAuthQrcodeUrl,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-  } = usePostApi<CreateOtpAuthQrcodeUrlReqBody, CreateOtpAuthQrcodeUrlResBody>(
-    '/auth/otp',
-    [['/auth/otp']]
-  );
-
+  const queryClient = useQueryClient();
   const { customToast } = useCustomToast();
-  useEffect(() => {
-    if (isError && isAxiosError<{ message: string }>(error)) {
-      customToast({ description: error.response?.data.message });
-    }
-  }, [isError, error, customToast]);
 
-  return { createOtpAuthQrcodeUrl, isLoading, isSuccess };
+  const { mutateAsync: createOtpAuthQrcodeUrl, ...useMutationResult } =
+    usePostApi<CreateOtpAuthQrcodeUrlReqBody, CreateOtpAuthQrcodeUrlResBody>(
+      '/auth/otp',
+      {
+        onSuccess: () => {
+          const queryKeys = [['/auth/otp']];
+          queryKeys.forEach((queryKey) => {
+            void queryClient.invalidateQueries({ queryKey });
+          });
+        },
+        onError: (error) => {
+          if (isAxiosError<{ message: string }>(error)) {
+            customToast({ description: error.response?.data.message });
+          }
+        },
+      }
+    );
+
+  return { createOtpAuthQrcodeUrl, ...useMutationResult };
 };
