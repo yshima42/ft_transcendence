1c387c8d2f91b7210eaaa813761ad4cbae5a7d9d
kohkubo <kohkubo@student.42tokyo.jp>
2023-01-08 12:27:50 +0900

    remove: Spinner

--- frontend/src/features/chat/components/atoms/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as C from '@chakra-ui/react';
-
-export const Spinner: React.FC = React.memo(() => (
-  <C.Center h="50vh">
-    <C.Spinner />
-  </C.Center>
-));
