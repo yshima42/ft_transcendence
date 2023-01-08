fe94e729e4d667420247dc535b1d385ce239a98e
kohkubo <kohkubo@student.42tokyo.jp>
2023-01-08 12:03:20 +0900

    テスト更新

--- backend/src/chat-message/chat-message.controller.ts
+++ backend/src/chat-message/chat-message.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Controller,
-  Get,
-  Param,
-  UseGuards,
-  ParseUUIDPipe,
-} from '@nestjs/common';
+import * as NestJs from '@nestjs/common';
 import * as Sw from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
@@ -12,15 +6,15 @@ import { JwtOtpAuthGuard } from 'src/auth/guards/jwt-otp-auth.guard';
 import { ResponseChatMessage } from './chat-message.interface';
 import { ChatMessageService } from './chat-message.service';
 
-@Controller('chat/rooms/:chatRoomId/messages')
+@NestJs.Controller('chat/rooms/:chatRoomId/messages')
 @Sw.ApiTags('chat-message')
-@UseGuards(JwtOtpAuthGuard)
+@NestJs.UseGuards(JwtOtpAuthGuard)
 export class ChatMessageController {
   constructor(private readonly chatMessageService: ChatMessageService) {}
 
-  @Get()
+  @NestJs.Get()
   async findAllNotBlocked(
-    @Param('chatRoomId', new ParseUUIDPipe()) chatRoomId: string,
+    @NestJs.Param('chatRoomId', new NestJs.ParseUUIDPipe()) chatRoomId: string,
     @GetUser() user: User
   ): Promise<ResponseChatMessage[]> {
     return await this.chatMessageService.findAllNotBlocked(chatRoomId, user.id);
