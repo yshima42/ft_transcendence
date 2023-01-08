e068b2a75ac3ec82342aaf2e36b601e35935243e
kohkubo <kohkubo@student.42tokyo.jp>
2023-01-08 17:31:16 +0900

    ブロック、ミュートだと遅れないように変更

--- backend/src/chat-message/chat-message.service.ts
+++ backend/src/chat-message/chat-message.service.ts
@@ -1,22 +1,45 @@
-import { Injectable, Logger } from '@nestjs/common';
+import * as NestJs from '@nestjs/common';
+import { ChatRoomMemberStatus } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ChatRoomMemberService } from '../chat-room-member/chat-room-member.service';
 import { ResponseChatMessage } from './chat-message.interface';
 import { CreateChatMessageDto } from './dto/create-chat-message.dto';
 
-@Injectable()
+@NestJs.Injectable()
 export class ChatMessageService {
-  constructor(private readonly prisma: PrismaService) {}
-  private readonly logger = new Logger('ChatMessageService');
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly chatRoomMemberService: ChatRoomMemberService
+  ) {}
+
+  private readonly logger = new NestJs.Logger('ChatMessageService');
   private readonly json = (obj: any): string => JSON.stringify(obj, null, 2);
 
   async create(
     createChatMessageDto: CreateChatMessageDto,
     senderId: string
   ): Promise<ResponseChatMessage> {
+    const { chatRoomId, content } = createChatMessageDto;
+    // ban || mute しているユーザーはメッセージを送信できない
+    const chatRoomMember = await this.chatRoomMemberService.findOne(
+      chatRoomId,
+      senderId
+    );
+    if (
+      chatRoomMember.memberStatus === ChatRoomMemberStatus.BANNED ||
+      chatRoomMember.memberStatus === ChatRoomMemberStatus.MUTED
+    ) {
+      // TODO ソケットなのでHttpのエラーではない
+      throw new NestJs.HttpException(
+        'You are banned or muted',
+        NestJs.HttpStatus.BAD_REQUEST
+      );
+    }
+
     const chatMessage = await this.prisma.chatMessage.create({
       data: {
-        content: createChatMessageDto.content,
-        chatRoomId: createChatMessageDto.chatRoomId,
+        content,
+        chatRoomId,
         senderId,
       },
       select: {
