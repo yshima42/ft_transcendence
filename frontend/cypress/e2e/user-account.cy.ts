9a35861c405cfe80294827e3c87d26c8cacdffc8
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-01-10 15:17:07 +0900

    デフォルトアバターの確認、コメント追加。

--- frontend/cypress/e2e/user-account.cy.ts
+++ frontend/cypress/e2e/user-account.cy.ts
@@ -2,12 +2,14 @@
 import { GameStats } from '../../src/hooks/api/game/useGameStats';
 
 describe('User Settings', function () {
+  // DBセットアップ
   before(() => {
     cy.exec(
       'yarn --cwd ../backend ts-node prisma/seed-cypress-user-account.ts'
     );
   });
 
+  // dummy1でログイン
   beforeEach(() => {
     cy.visit('/');
     cy.getBySel('dummy1-login').click();
@@ -17,24 +19,48 @@ describe('User Settings', function () {
     cy.location('pathname').should('eq', '/app');
   });
 
+  /**
+   * シナリオ UA-1
+   * サイドバーのアバターからプロフィールページへ移動。
+   * Editボタンクリックして、ProfileEdit表示。
+   * ニックネームを変更し、表示の変更を確認。
+   *
+   * チェック項目No.17, 58
+   */
   it('ユーザーは、ウェブサイトに表示される一意の名前を選択できるようにする必要がある（ニックネーム）', () => {
     cy.getBySel('sidenav-user-avatar').click();
-    cy.location('pathname').should('eq', '/app/profile');
+    cy.location('pathname').should('contain', '/app/users');
     cy.getBySel('profile-edit').click();
     cy.getBySelLike('nickname-input').clear().type('New Nickname');
     cy.getBySelLike('submit').click();
     cy.getBySel('profile-nickname').should('contain', 'New Nickname');
   });
 
+  /**
+   * シナリオ UA-2
+   * サイドバーのアバターからプロフィールページへ移動。
+   * デフォルトアバターが設定されているか確認。
+   * Editボタンクリックして、ProfileEdit表示。
+   * アバターをアップロードし、パスの変更を確認。
+   *
+   * チェック項目No.18, 19, 58
+   */
   it('ユーザーは、アバターをアップロードすることができる', () => {
     const defaultAvatarPath =
-      'https://placehold.jp/2b52ee/ffffff/150x150.png?text=dummy1';
+      'https://placehold.jp/0C163D/fffffe/150x150.png?text=dummy1';
     const uploadFilePath = 'cypress/fixtures/zazen_obousan.png';
     const partOfUploadDirPath = 'avatar/dummy1';
 
     cy.getBySel('sidenav-user-avatar').click();
-    cy.location('pathname').should('eq', '/app/profile');
+    cy.location('pathname').should('contain', '/app/users');
 
+    // デフォルトアバターが設定されているか確認。
+    cy.getBySel('profile-user-avatar')
+      .children('img.chakra-avatar__img')
+      .should('have.attr', 'src')
+      .and('have.string', defaultAvatarPath);
+
+    // アバターアップロード
     cy.getBySel('profile-edit').click();
     cy.getBySelLike('file-select').selectFile(uploadFilePath);
     cy.getBySelLike('submit').click();
@@ -49,9 +75,16 @@ describe('User Settings', function () {
       .and('have.string', partOfUploadDirPath);
   });
 
+  /**
+   * シナリオ UA-3
+   * サイドバーのアバターからプロフィールページへ移動。
+   * Stats, MatchHistoryの確認をする。
+   *
+   * チェック項目No.22, 23
+   */
   it('統計情報（勝敗、ラダーレベル(勝率)、実績など）がユーザープロフィールに表示される', () => {
     cy.getBySel('sidenav-user-avatar').click();
-    cy.location('pathname').should('eq', '/app/profile');
+    cy.location('pathname').should('contain', '/app/users');
 
     // ゲームの結果（最新5件）が表示されているか確認
     // バックエンドのデータと一致しているか確認するのは大変なので、
@@ -73,6 +106,13 @@ describe('User Settings', function () {
     });
   });
 
+  /**
+   * シナリオ UA-4
+   * サイドバーからFriendタブを表示。
+   * Friendの一覧を取得し、バッチの表示を確認する。
+   *
+   * チェック項目No.21, 61
+   */
   it('フレンドインターフェースでは、フレンドとその状態（オフライン／オンライン／ゲーム中等）を確認することができる', () => {
     cy.getBySel('sidenav-users').click();
     cy.getBySel('users-friends-tab').click();
@@ -86,7 +126,16 @@ describe('User Settings', function () {
     });
   });
 
-  it.only('他のユーザーをブロックすることができる。', () => {
+  /**
+   * シナリオ UA-5
+   * サイドバーからFriendタブを表示。
+   * Friendの中から一人選び、アバターからプロフィールへ移動。
+   * プロフィールページでブロックボタンをクリック。
+   * Friendタブの、ブロック一覧に存在することを確認。
+   *
+   * チェック項目No.27, 60
+   */
+  it('他のユーザーをブロックすることができる。', () => {
     cy.getBySel('sidenav-users').click();
     cy.getBySel('users-friends-tab').click();
     cy.location('pathname').should('eq', '/app/users');
