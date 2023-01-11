f2447754af56a83e40008c3d81bf0758f89e620f
yuki <shimazuyuki7@gmail.com>
2023-01-11 10:44:15 +0900

    fix: match history修正

--- frontend/src/features/profile/components/organisms/MatchHistoryCard.tsx
+++ frontend/src/features/profile/components/organisms/MatchHistoryCard.tsx
@@ -1,5 +1,5 @@
 import { memo, FC } from 'react';
-import { Box, Stack, Text, VStack } from '@chakra-ui/react';
+import { Box, Flex, Stack, Text, VStack } from '@chakra-ui/react';
 import { useMatchHistory } from 'hooks/api/game/useMatchHistory';
 import { GameResultCard } from '../molecules/GameResultCard';
 
@@ -18,30 +18,44 @@ export const MatchHistoryCard: FC<Props> = memo((props) => {
   return (
     <>
       {/* TODO:spinnerつける？使うフックを変更し、一時的にspinner表示を削除した */}
-      <Box bg="gray.200" borderRadius="20px" p={3}>
+      <Flex
+        w="100%"
+        h="100%"
+        bg="gray.200"
+        borderRadius="20px"
+        shadow="md"
+        p={3}
+        pt={5}
+        direction="column"
+      >
         <Stack>
-          <Box pl={3} pb={4}>
+          <Box pl={3} pb={2}>
             <Text as="b">Match History</Text>
           </Box>
           <VStack justify="center" data-test="profile-latest-5matches">
-            {latest5Matches.map((match) => (
-              <Box key={match.id}>
-                <GameResultCard
-                  matchId={match.id}
-                  playerOne={match.playerOne}
-                  playerTwo={match.playerTwo}
-                  score={`${match.playerOneScore} - ${match.playerTwoScore}`}
-                  createdAt={match.finishedAt}
-                  win={
-                    (id === match.playerOne.id && match.playerOneScore === 5) ||
-                    (id === match.playerTwo.id && match.playerTwoScore === 5)
-                  }
-                />
-              </Box>
-            ))}
+            {latest5Matches.length === 0 ? (
+              <Flex>No Data</Flex>
+            ) : (
+              latest5Matches.map((match) => (
+                <Box key={match.id}>
+                  <GameResultCard
+                    matchId={match.id}
+                    playerOne={match.playerOne}
+                    playerTwo={match.playerTwo}
+                    score={`${match.playerOneScore} - ${match.playerTwoScore}`}
+                    createdAt={match.finishedAt}
+                    win={
+                      (id === match.playerOne.id &&
+                        match.playerOneScore === 5) ||
+                      (id === match.playerTwo.id && match.playerTwoScore === 5)
+                    }
+                  />
+                </Box>
+              ))
+            )}
           </VStack>
         </Stack>
-      </Box>
+      </Flex>
     </>
   );
 });
