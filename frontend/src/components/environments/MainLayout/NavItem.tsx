70200f2f5f5d2d11355dd988a19ffd6de430207a
yuki <shimazuyuki7@gmail.com>
2023-01-12 10:54:44 +0900

    fix: サイドバー少し修正

--- frontend/src/components/environments/MainLayout/NavItem.tsx
+++ frontend/src/components/environments/MainLayout/NavItem.tsx
@@ -12,19 +12,19 @@ export const NavItem: FC<Props> = memo((props) => {
   const { title, icon, to } = props;
 
   return (
-    <Flex mt={30} flexDir="column" w="100%" alignItems="flex-start">
+    <Flex mt={5} flexDir="column" w="100%" alignItems="flex-start">
       <Menu placement="right">
         <Link to={to}>
           <Flex
             p={3}
-            borderRadius={8}
-            _hover={{ textDecor: 'none', backgroundColor: '#AEC8CA' }}
+            borderRadius={100}
+            w="160px"
+            _hover={{
+              textDecor: 'none',
+              backgroundColor: 'gray.100',
+            }}
           >
-            <MenuButton
-              w="100%"
-              _hover={{ textDecor: 'none', backgroundColor: '#AEC8CA' }}
-              data-test={'sidenav-' + title.toLowerCase()}
-            >
+            <MenuButton w="100%" data-test={'sidenav-' + title.toLowerCase()}>
               <Flex>
                 <Icon as={icon} fontSize="xl" color={'gray.700'} />
                 <Text ml={5} display={{ base: 'none', md: 'flex' }}>
