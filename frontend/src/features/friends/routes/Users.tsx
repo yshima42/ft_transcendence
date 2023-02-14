a24daf4a9f11234ce0c0d21158816572c2c671d3
mkamei <mkamei@student.42tokyo.jp>
2023-02-14 11:35:50 +0900

    users ページから、block 関係削除

--- frontend/src/features/friends/routes/Users.tsx
+++ frontend/src/features/friends/routes/Users.tsx
@@ -2,18 +2,16 @@ import { memo, FC, Suspense } from 'react';
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import { CenterSpinner } from 'components/atoms/spinner/CenterSpinner';
 import { ContentLayout } from 'components/ecosystems/ContentLayout';
-import { BlockedList } from 'features/friends/components/organisms/BlockedList';
 import { FriendsList } from 'features/friends/components/organisms/FriendsList';
 import { PendingList } from 'features/friends/components/organisms/PendingList';
 import { RecognitionList } from 'features/friends/components/organisms/RecognitionList';
 import { RequestableUsersList } from 'features/friends/components/organisms/RequestableUsersList';
 
-const tabs = ['Friends', 'Pending', 'Recognition', 'Blocked', 'AddFriend'];
+const tabs = ['Friends', 'Pending', 'Recognition', 'AddFriend'];
 const dataTestList = [
   'users-friends-tab',
   'users-pending-tab',
   'users-recognition-tab',
-  'users-blocked-tab',
   'users-add-friend-tab',
 ];
 
@@ -38,9 +36,6 @@ export const Users: FC = memo(() => {
             </TabPanel>
             <TabPanel>
               <RecognitionList />
-            </TabPanel>
-            <TabPanel>
-              <BlockedList />
             </TabPanel>
             <TabPanel>
               <RequestableUsersList />
